be1b6b88bc8a41dfb789f8bfc03e9b89a0eea5a5
Sagit Gutierrez <jozaguts@gmail.com>
2023-03-23 17:31:02 -0600

    feat(enviromen) : server vars added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
 
+export default defineNuxtConfig({
+  modules: ['nuxt-typed-router'],
+  runtimeConfig: {
+    public:{
+      baseURLBackend: process.env.NUXT_PUBLIC_URL_BACKEND,
+      backendPrefix: process.env.NUXT_PUBLIC_BACKEND_PREFIX,
+    }
+  }
 })
