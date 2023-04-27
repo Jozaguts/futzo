9a821518bce79b13e2d53886f144cc70d0fafda2
Sagit Gutierrez <jozaguts@gmail.com>
2023-04-27 13:33:19 -0600

    feat(vuetify): added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
-  modules: ['nuxt-typed-router'],
+  modules: ['nuxt-typed-router','@invictus.codes/nuxt-vuetify'],
+  vuetify: {
+    /* vuetify options */
+    vuetifyOptions: {
+      // @TODO: list all vuetify options
+    },
+    moduleOptions: {
+      /* nuxt-vuetify module options */
+      treeshaking: true,
+      useIconCDN: false,
+      /* vite-plugin-vuetify options */
+      // styles: true | 'none' | 'expose' | 'sass' | { configFile: string },
+      styles: true,
+      autoImport: true,
+    }
+  },
   runtimeConfig: {
     public:{
       baseURLBackend: process.env.NUXT_PUBLIC_URL_BACKEND,
