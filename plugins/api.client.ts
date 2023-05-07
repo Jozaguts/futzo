6d98705bc7cff97ad2d9036ecd30c98b9df3b8f0
Sagit Gutierrez <jozaguts@gmail.com>
2023-05-07 13:01:33 -0600

    sidebase/nuxt-auth removed

--- plugins/api.client.ts
+++ plugins/api.client.ts
@@ -1,43 +1,33 @@
 import {defineNuxtPlugin} from '#app';
 import {AuthModule} from "~/repositories/modules/auth";
-import {IApiInstance} from "~/interfaces";
+import {Auth, IApiInstance} from "~/interfaces";
 import {FetchContext, FetchOptions} from "ofetch/dist/node";
 import Cookies from "js-cookie";
 import {$fetch } from 'ofetch'
 export default defineNuxtPlugin((nuxtApp) => {
-
-  const fetchOption: FetchOptions = {
-    baseURL: nuxtApp.$config.public.baseURLBackend,
-    credentials: 'include',
-    // @ts-ignore
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json',
-      'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN'),
-    },
-    onRequest(context: FetchContext): Promise<void> | void  {
+  const fetchOption = (): FetchOptions => {
+    return {
+      baseURL: nuxtApp.$config.public.baseURLBackend,
+      credentials: 'include',
       // @ts-ignore
-      context.options.headers['X-XSRF-TOKEN'] = Cookies.get('XSRF-TOKEN')
-
-    },
-    onResponse(context: FetchContext): Promise<void> | void {
-    },
-    onRequestError(context: FetchContext): Promise<void> | void {
-    },
-    onResponseError(context: FetchContext): Promise<void> | void {
-
-    },
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN'),
+        'Authorization': 'Bearer ' + useCookie('XSRF-TOKEN').value,
+      },
+    }
   }
 
-  const apiFetcher =  $fetch.create(fetchOption);
+  const apiFetcher =  $fetch.create(fetchOption());
 
   const modules: IApiInstance = {
     auth: new AuthModule(apiFetcher),
-  }
+      }
 
   return {
     provide: {
-      api: modules
+      api: modules,
     }
   }
 
