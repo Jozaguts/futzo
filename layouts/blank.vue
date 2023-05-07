7e2eb7235356d920c08de1d791c968a644743f2f
Sagit Gutierrez <jozaguts@gmail.com>
2023-05-07 13:03:39 -0600

    feat(layouts): initial layouts added

---
+++ layouts/blank.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-app class="layout-wrapper layout-nav-type-vertical">
+        <shared-loader v-if="useGlobalStore().isLoading"/>
+        <v-main class="layout-wrapper layout-blank" v-else>
+            <slot />
+        </v-main>
+    </v-app>
+</template>
+<script setup lang="ts" >
+import { useGlobalStore } from "~/store";
+
+onMounted(() => {
+    useGlobalStore().isLoading = false
+})
+</script>
+<style>
+.layout-wrapper.layout-blank {
+    flex-direction: column;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,31 @@
 <template>
 <div>
-    <slot />
+    <shared-loader v-if="useGlobalStore().isLoading"/>
+    <v-app v-else class="layout-wrapper layout-nav-type-vertical">
+        <v-app-bar ></v-app-bar>
+        <v-navigation-drawer >
+            <v-btn @click="$router.push('/login')"> login</v-btn>
+            <v-btn @click="$router.push('/')"> home</v-btn>
+           <v-btn   @click="useNuxtApp().$api.auth.logout()">logout</v-btn>
+        </v-navigation-drawer>
+        <v-main>
+            default layout
+            <slot />
+        </v-main>
+        <v-footer>
+            footer
+        </v-footer>
+    </v-app>
 </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useGlobalStore } from "~/store";
+useGlobalStore().$state.isLoading = true
+onMounted(() => {
+    useGlobalStore().$state.isLoading = false
+})
+</script>
+<style lang="scss">
+@use "src/@layouts/styles/default-layout";
+</style>
 
