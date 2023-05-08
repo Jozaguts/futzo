81abbc5700409197030a1f81ea6b9e3e178bc250
Sagit Gutierrez <jozaguts@gmail.com>
2023-05-08 01:20:38 -0600

    master branch

--- deploy.sh
+++ deploy.sh
@@ -10,8 +10,7 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/Jozaguts/futzo.git main:web
+git push -f https://github.com/Jozaguts/futzo.git master:web
 
 cd -
 rm -rf /output
