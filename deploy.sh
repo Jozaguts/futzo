d7fcedf92de97b3d93e5a6765c8937fb4a3ba6de
Sagit Gutierrez <jozaguts@gmail.com>
2023-05-08 01:13:56 -0600

    deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+cp ecosystem.config.js .output/
+cd .output/
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/Jozaguts/futzo.git main:web
+
+cd -
+rm -rf /output
