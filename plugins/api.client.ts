cefe6f44e68310c4060187b4d1592c23bc71dc6a
Jose Terrazas <jozaguts@gmail.com>
2023-09-24 16:58:57 -0600

    feat(config) plugin config added

--- plugins/api.client.ts
+++ plugins/api.client.ts
@@ -4,20 +4,23 @@ import { IApiInstance} from "~/interfaces";
 import { FetchOptions} from "ofetch/dist/node";
 import Cookies from "js-cookie";
 import {$fetch } from 'ofetch'
+import {useLocalStorage} from "@vueuse/core";
 export default defineNuxtPlugin((nuxtApp) => {
   const config = nuxtApp.$config.public
-  const fetchOption = (): FetchOptions => {
-    console.log(`${config.baseURLBackend}/${config.backendPrefix}`,)
+  const fetchOption = (): { headers: { Authorization: string; Accept: string }; baseURL: string; credentials: string } => {
+    let headers = {}
+    const token =  useLocalStorage('token',null).value
+    if (token){
+      headers = {
+        'Authorization': `Bearer ${token}`,
+      }
+    }
+    // @ts-ignore
     return {
       baseURL: `${config.baseURLBackend}/${config.backendPrefix}`,
-      credentials: 'include',
-      // @ts-ignore
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN'),
-        'Authorization': 'Bearer ' + useCookie('XSRF-TOKEN').value,
-      },
+        headers: {
+          ...headers
+        }
     }
   }
 
