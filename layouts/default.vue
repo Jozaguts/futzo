17e88461b6ef1a2e611012c491eb7a6ef5a863db
Jose Terrazas <jozaguts@gmail.com>
2024-02-11 18:10:13 -0600

    feat(css): v-list-item bg color added

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,3 +15,11 @@
 import Navigation from '~/components/layout/navigation-drawer.vue'
 import AppBar from  '~/components/layout/app-bar.vue'
 </script>
+<style>
+ .v-list-item--active {
+  background: linear-gradient(-72.47deg, rgb(145, 85, 253) 22.16%, rgba(145, 85, 253, 0.7) 76.47%) !important;
+}
+.v-list-item:hover, .v-list-item--active {
+  border-radius: 25px;
+}
+</style>
