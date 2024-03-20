cdfc7fa76fc2d34fea38309d2aee6f8284ebfd59
Jose Terrazas <jozaguts@gmail.com>
2024-03-19 18:32:28 -0600

    feat(loader): image added

--- server/plugins/01-extend-html.ts
+++ server/plugins/01-extend-html.ts
@@ -1,20 +1,12 @@
 
 export default defineNitroPlugin((nitroApp) => {
     nitroApp.hooks.hook('render:html', (html, { event }) => {
-        // This will be an object representation of the html template.
-        // const loaderColor  = window.localStorage('futzo-initial-loader-bg') ?? '#FFFFFF'
-        // const primaryColor = window.localStorage('futzo-initial-loader-color') ?? '#9155FD'
-        html.bodyPrepend.push(`<div id="loading-bg">
-            <div class="loading-logo">
-                <img src="/logo.png" height="80" alt="Logo" />
-                <h1 class="text-center">Futzo</h1>  
+        html.bodyPrepend.push(`
+            <div id="loading-bg">
+            <div class="loading-logo animated-element">
+                <img src="/futzo/logos/circular/logo-22.png" height="180" alt="Logo" />
             </div>
-            <div class="loading">
-                <div class="effect-1 effects"></div>
-                <div class="effect-2 effects"></div>
-                <div class="effect-3 effects"></div>
-            </div>
-        </div>`)
+            </div>`)
         html.head.push(`
         <script async>
         window.addEventListener('load', function () {
