82012ecd51df561f5b986d78dcc86d3fd7781a7f
Jose Terrazas <jozaguts@gmail.com>
2024-05-17 16:15:23 -0600

    fix(user): refresh user

--- pages/authorize/[...provider]/callback.vue
+++ pages/authorize/[...provider]/callback.vue
@@ -16,12 +16,22 @@ onMounted(() => {
   const provider = useRoute().params.provider[0]
   if ( provider === 'google' || provider === 'facebook') {
     const client = useSanctumClient()
-    client( `/auth/${provider}/callback`,{params: {code : useRoute().query.code}})
-        .then(response =>{
-          if (response.success) {
-            useLocalStorage('token', response.token)
+    client( `/auth/${provider}/callback`,{
+      credentials: 'include',
+      params: {code : useRoute().query.code}
+    })
+        .then(() =>{
+          const {refreshIdentity, user, isAuthenticated} = useSanctumAuth()
+          refreshIdentity().catch(error => console.error(error))
+          if (isAuthenticated.value) {
+            console.log('user authenticated')
             useRouter().push('/')
+          }else {
+            console.log('user not authenticated')
+            user.value.name = 'test'
+            user.value.id = 1
           }
+
         }).catch(error => console.error(error))
   } else{
     console.error('provider not found')
