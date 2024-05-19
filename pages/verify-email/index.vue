ed7c1536abd3b38d3bb79d3c797da5f1c1b11590
Jose Terrazas <jozaguts@gmail.com>
2024-05-19 03:34:03 -0600

    fix(mail verification): take token and make request validation

---
+++ pages/verify-email/index.vue
@@ -0,0 +1,53 @@
+<script lang="ts" setup>
+
+definePageMeta({
+  layout: 'blank',
+  bodyAttrs: {
+    class: 'd-none'
+  }
+});
+const status = ref({
+  message: 'verificando...',
+  status: 200,
+})
+const queryString = useRoute().query
+const client = useSanctumClient();
+const routerId = ref( null as number)
+const verifyEmail = async () =>{
+  return await client(`/verify-email?token=${queryString.token}`)
+      .then(() => {
+        status.value.message = 'Correo verificado'
+        status.value.status = 200
+      })
+      .catch((error) => {
+        status.value.message = error.data.message
+      })
+      .finally(() => {
+        if (status.value.status === 200  || status.value.message === 'Email already verified.') {
+          status.value.message = 'Redireccionando...'
+          routerId.value =   setTimeout(() => {
+            useRouter().push({name: 'ligas'})
+          }, 2000)
+        }
+      })
+}
+onMounted(() => {
+  verifyEmail()
+});
+onUnmounted(() => {
+  clearTimeout(routerId.value)
+});
+</script>
+<template>
+  <div class="verify-container">
+    <h1>{{status.message}}</h1>
+  </div>
+</template>
+<style>
+  .verify-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>
