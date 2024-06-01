5a8f8feb686273bfcd3596dc67a0edb708fa562a
Jose Terrazas <jozaguts@gmail.com>
2024-06-01 16:29:18 -0600

    chore(dependencies): laravel-sanctum updated

--- vuetify.config.ts
+++ vuetify.config.ts
@@ -1,10 +1,13 @@
 import { defineVuetifyConfiguration } from 'vuetify-nuxt-module/custom-configuration'
 import theme from './config/vuetify/theme'
 import defaults from './config/vuetify/defaults'
-
 export default defineVuetifyConfiguration({
     /* vuetify options */
     theme,
     defaults,
+    icons: {
+        defaultSet: 'mdi',
+        sets: ['mdi'],
+    }
 
 })
