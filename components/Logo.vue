041e7e90ab588d44423168e474531fe20332a4bf
Jose Terrazas <jozaguts@gmail.com>
2024-06-01 17:37:31 -0600

    feat(page): bienvenido page added

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,11 +1,17 @@
 <script lang="ts" setup>
 import {useTheme} from "vuetify";
 const logo = ref('/futzo/logos/horizontal/logo-12.png')
+export interface Props {
+  maxWidth?: string | number
+}
 
+const props = withDefaults(defineProps<Props>(), {
+  maxWidth: '100%',
+})
 watch(useTheme().global.current, (value) => {
   value.dark ? logo.value = '/futzo/logos/horizontal/logo-14.png' : logo.value = '/futzo/logos/horizontal/logo-12.png'
 }, {deep: true})
 </script>
 <template>
-  <v-img :src="logo"></v-img>
+  <v-img :max-width="props.maxWidth" :src="logo"></v-img>
 </template>
--- pages/bienvenido.vue
+++ pages/bienvenido.vue
@@ -9,5 +9,56 @@ definePageMeta({
 </script>
 
 <template>
-  <h1>bienvenido</h1>
+  <v-sheet height="100%" class="d-flex justify-center align-center">
+    <v-card rounded border color="background" class="w-lg-50 w-md-50" >
+     <div class="logo-container" >
+       <Logo max-width="165" />
+     </div>
+      <v-card-item class="d-flex align-center justify-center">
+        <v-card-title>
+          <h1 class="font-weight-bold text-h5">Vamos a crear tu primera liga</h1>
+        </v-card-title>
+      </v-card-item>
+      <v-card-text>
+       <v-form class="w-50 mx-auto">
+        <v-text-field
+            label="Nombra tu liga"
+            variant="outlined"
+            hint="Ejemplo: Liga de amigos"
+            persistent-hint
+        >
+          <template #append-inner>
+            <Icon name="ph:question" size="20"></Icon>
+          </template>
+        </v-text-field>
+       </v-form>
+        <v-card-actions>
+          <v-btn
+              class="ml-auto text-capitalize"
+              color="primary"
+              variant="elevated"
+              size="large"
+              density="default"
+          >
+            Crear liga
+          </v-btn>
+        </v-card-actions>
+      </v-card-text>
+    </v-card>
+  </v-sheet>
 </template>
+<style >
+footer{
+  display: none !important;
+}
+.logo-container {
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  text-align: center;
+}
+
+
+</style>
