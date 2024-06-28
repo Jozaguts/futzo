cb4edb91aaf648f65caec93b8346696479a0ae0b
Jose Terrazas <jozaguts@gmail.com>
2024-06-28 03:41:08 -0600

    refactor(middleware):has league

--- middleware/02-user-has-leauge.global.ts
+++ middleware/02-user-has-leauge.global.ts
@@ -1,13 +1,9 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     if (process.client){
         const user = useSanctumUser()
-        // Comprueba si la ruta actual es la de verificación de correo
-        const isVerificationRoute = to.path.startsWith('/verify-email/');
-        // Si es la ruta de verificación, permite el acceso
-        if (isVerificationRoute  || user.value?.roles.includes('super administrador')) {
+          if (user.value?.roles.includes('super administrador')) {
             return;
         }
-
 
         if (to.path !== '/bienvenido' && user.value?.league === 'No league assigned') {
             return navigateTo({name: "bienvenido"})
