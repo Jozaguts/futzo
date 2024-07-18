0fe6f4954c14a638ade01bffb8552fee51d72072
Jose Terrazas <jozaguts@gmail.com>
2024-07-17 23:03:38 -0600

    feat(components ): edit user component added

---
+++ components/configuration/personal-data-card.vue
@@ -0,0 +1,119 @@
+<script setup lang="ts">
+import { VPhoneInput } from "v-phone-input";
+import { useAuthStore } from "~/store";
+import type { User } from "~/models/user";
+
+const { fields, resetForm, handleSubmit } = useSchemas("edit-user");
+const user = computed(() => useAuthStore().user as User);
+onMounted(() => {
+  resetForm({
+    values: {
+      name: user.value.name,
+      phone: user.value.phone,
+      email: user.value.email,
+    },
+  });
+});
+const submit = handleSubmit(async (values) => {
+  console.log(values);
+});
+</script>
+<template>
+  <v-card class="secondary-card" variant="text">
+    <v-card-item class="secondary-card-item">
+      <v-card-text class="secondary-card__title"
+        >Edita tus datos personales</v-card-text
+      >
+      <v-card-subtitle class="secondary-card__subtitle">
+        Estos son tus datos personales, puedes editarlos
+        debajo.</v-card-subtitle
+      >
+    </v-card-item>
+    <v-card-text>
+      <v-form class="user-data-configuration-form" @submit.prevent="submit">
+        <v-row class="row-border-bottom" no-gutters>
+          <v-col cols="3">
+            <p class="label-form">Nombre completo</p>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              v-model="fields.name.fieldValue"
+              variant="plain"
+              class="user-data-configuration-form__input"
+            ></v-text-field>
+            <small class="text-error">{{
+              fields.name.fieldPropsValue["error-messages"][0]
+            }}</small>
+          </v-col>
+        </v-row>
+        <v-row class="row-border-bottom" no-gutters>
+          <v-col cols="3">
+            <p class="label-form">Teléfono</p>
+          </v-col>
+          <v-col cols="4">
+            <VPhoneInput
+              variant="plain"
+              :singleLine="true"
+              v-model="fields.phone.fieldValue"
+              class="user-data-configuration-form__input"
+              :invalidMessage="
+                ({ label, example }) => {
+                  return `${label} debe ser un numero valido (${example}).`;
+                }
+              "
+            >
+            </VPhoneInput>
+            <small class="text-error">{{
+              fields.phone.fieldPropsValue["error-messages"][0]
+            }}</small>
+          </v-col>
+        </v-row>
+        <!--        <v-row class="row-border-bottom" no-gutters>-->
+        <!--          <v-col cols="3">-->
+        <!--            <p class="label-form">Ciudad</p>-->
+        <!--          </v-col>-->
+        <!--          <v-col cols="4">-->
+        <!--            <v-text-field-->
+        <!--              v-model="fields.city.fieldValue"-->
+        <!--              variant="plain"-->
+        <!--              class="user-data-configuration-form__input"-->
+        <!--            ></v-text-field>-->
+        <!--            <small class="text-error">{{-->
+        <!--              fields.city.fieldPropsValue["error-messages"][0]-->
+        <!--            }}</small>-->
+        <!--          </v-col>-->
+        <!--        </v-row>-->
+        <v-row class="row-border-bottom" no-gutters>
+          <v-col cols="3">
+            <p class="label-form">Correo electrónico</p>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              type="email"
+              v-model="fields.email.fieldValue"
+              variant="plain"
+              class="user-data-configuration-form__input"
+            ></v-text-field>
+            <small class="text-error">{{
+              fields.email.fieldPropsValue["error-messages"][0]
+            }}</small>
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col cols="4" offset="3">
+            <div class="d-flex justify-end align-center pt-4">
+              <v-btn
+                type="submit"
+                class="user-data-configuration-form__button"
+                color="primary"
+                dark
+              >
+                Guardar cambios
+              </v-btn>
+            </div>
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-card-text>
+  </v-card>
+</template>
--- composables/useSchemas.ts
+++ composables/useSchemas.ts
@@ -1,105 +1,138 @@
-import {useForm} from 'vee-validate';
-import * as yup from 'yup';
+import { useForm } from "vee-validate";
+import * as yup from "yup";
+
 export default function (schemaNAme: string) {
-    const vuetifyConfig = (state) => {
-        return {
-            props: {
-                'error-messages': state.errors,
-            },
-        }
-    };
-
-    const schema = getSchemaByName(schemaNAme);
-
-    const fields = Object.keys(schema.fields);
-
-    const { defineField, handleSubmit, resetForm } = useForm({
-        validationSchema: schema,
-    });
-    const fieldProps = reactive({});
-
-    fields.forEach(field => {
-        const [fieldValue, fieldPropsValue] = defineField(field, vuetifyConfig);
-        fieldProps[field] = { fieldValue, fieldPropsValue };
-    });
-
+  const vuetifyConfig = (state) => {
     return {
-        handleSubmit,
-        resetForm,
-        fields: fieldProps,
-    }
+      props: {
+        "error-messages": state.errors,
+      },
+    };
+  };
+
+  const schema = getSchemaByName(schemaNAme);
+
+  const fields = Object.keys(schema.fields);
+
+  const { defineField, handleSubmit, resetForm } = useForm({
+    validationSchema: schema,
+  });
+  const fieldProps = reactive({});
+
+  fields.forEach((field) => {
+    const [fieldValue, fieldPropsValue] = defineField(field, vuetifyConfig);
+    fieldProps[field] = { fieldValue, fieldPropsValue };
+  });
+
+  return {
+    handleSubmit,
+    resetForm,
+    fields: fieldProps,
+  };
 }
 
 function getSchemaByName(name) {
-    let schemaFields = {};
-    const {t} = useI18n();
-    const yusString =  () =>{
-        return yup.string().test('no-leading-space', 'No se permite espacio en blanco al inicio', value => {
-                if (value && value.startsWith(' ')) {
-                    return false;
-                }
-                return true;
-            }
+  let schemaFields = {};
+  const { t } = useI18n();
+  const yusString = () => {
+    return yup
+      .string()
+      .test(
+        "no-leading-space",
+        "No se permite espacio en blanco al inicio",
+        (value) => {
+          if (value && value.startsWith(" ")) {
+            return false;
+          }
+          return true;
+        },
+      );
+  };
+  switch (name) {
+    case "create-tournament":
+      schemaFields.name = yusString()
+        .min(6, t("tournament_min"))
+        .required(t("forms.required"));
+      schemaFields.image = yup
+        .mixed()
+        .test("File is required", "Campo requerido ", (value: File) => value);
+      schemaFields.category_id = yup.number().required(t("forms.required"));
+      schemaFields.tournament_format_id = yup
+        .number()
+        .required(t("forms.required"));
+      schemaFields.location = yup.object(); // club/lugar
+      schemaFields.city = yusString().required(t("forms.required"));
+      schemaFields.address = yusString().required(t("forms.required"));
+      schemaFields.start_date = yup.date().nullable();
+      schemaFields.end_date = yup.date().nullable();
+      schemaFields.prize = yusString().required(t("forms.required"));
+      schemaFields.winner = yusString().nullable();
+      schemaFields.description = yusString().nullable();
+      schemaFields.status = yusString().nullable();
+      break;
+    case "create-league":
+      schemaFields.id = yusString().nullable();
+      schemaFields.name = yusString()
+        .min(6, t("league_min"))
+        .required(t("forms.required"));
+      schemaFields.location = yusString().nullable();
+      schemaFields.description = yusString().nullable();
+      schemaFields.creation_date = yup.date().nullable();
+      schemaFields.football_type_id = yup
+        .number()
+        .required(t("forms.required"));
+      schemaFields.logo = yup
+        .mixed()
+        .test("File is required", "File ", (value: File) => value);
+      schemaFields.banner = yup
+        .mixed()
+        .test("File is required", "File ", (value: File) => value);
+      schemaFields.status = yusString().nullable();
+      break;
+    case "create-category":
+      schemaFields.name = yusString().required(t("forms.required"));
+      schemaFields.age_range = yusString()
+        .matches(
+          /^(\d{2}-\d{2}|\*)$/,
+          "El formato debe ser 'NN-NN' donde N es un dígito, o '*' para edad libre.",
         )
-    }
-    switch (name) {
-        case 'create-tournament':
-            schemaFields.name = yusString().min(6, t('tournament_min')).required(t('forms.required'));
-            schemaFields.image = yup.mixed()
-                .test('File is required', 'Campo requerido ', (value: File) => value)
-            schemaFields.category_id = yup.number().required(t('forms.required'));
-            schemaFields.tournament_format_id = yup.number().required(t('forms.required'));
-            schemaFields.location = yup.object() // club/lugar
-            schemaFields.city = yusString().required(t('forms.required'));
-            schemaFields.address = yusString().required(t('forms.required'));
-            schemaFields.start_date = yup.date().nullable();
-            schemaFields.end_date = yup.date().nullable();
-            schemaFields.prize =   yusString().required(t('forms.required'));
-            schemaFields.winner = yusString().nullable();
-            schemaFields.description = yusString().nullable();
-            schemaFields.status = yusString().nullable();
-            break;
-        case 'create-league':
-            schemaFields.id = yusString().nullable();
-            schemaFields.name = yusString().min(6, t('league_min')).required(t('forms.required'));
-            schemaFields.location = yusString().nullable();
-            schemaFields.description = yusString().nullable();
-            schemaFields.creation_date = yup.date().nullable();
-            schemaFields.football_type_id = yup.number().required(t('forms.required'));
-            schemaFields.logo = yup.mixed()
-                .test('File is required', 'File ', (value: File) => value)
-            schemaFields.banner = yup.mixed()
-                .test('File is required', 'File ', (value: File) => value)
-            schemaFields.status = yusString().nullable();
-            break;
-        case 'create-category':
-            schemaFields.name = yusString().required(t('forms.required'));
-            schemaFields.age_range = yusString()
-                .matches(/^(\d{2}-\d{2}|\*)$/, "El formato debe ser 'NN-NN' donde N es un dígito, o '*' para edad libre.")
-                .test('es-rango-valido-o-libre', 'El primer número debe ser menor que el segundo, o usar "*" para edad libre', (value) => {
-                    if (!value) return false;
-                    if (value === '*') return true;
-                    const [inicio, fin] = value.split('-').map(Number);
-                    return inicio < fin; // Retorna true si el primer número es menor que el segundo
-                });
-            schemaFields.gender = yusString().required(t('forms.required'));
-            break;
-        case 'create-team':
-            schemaFields.name = yusString().required(t('forms.required'));
-            schemaFields.category_id = yup.number().required(t('forms.required'));
-            schemaFields.tournament_id = yup.number().required(t('forms.required'));
-            schemaFields.president_name = yusString().required(t('forms.required'));
-            schemaFields.coach_name = yusString().required(t('forms.required'));
-            schemaFields.phone = yusString().matches(/^\d{10}$/, 'Número de teléfono no es válido').required();
-            schemaFields.email = yusString().email();
-            schemaFields.address = yusString();
-            schemaFields.image = yup.array().nullable();
-            schemaFields.location_id = yup.number().nullable();
-            break;
-            default:
-                schemaFields = yup.mixed();
-
-    }
-    return yup.object().shape(schemaFields);
-
+        .test(
+          "es-rango-valido-o-libre",
+          'El primer número debe ser menor que el segundo, o usar "*" para edad libre',
+          (value) => {
+            if (!value) return false;
+            if (value === "*") return true;
+            const [inicio, fin] = value.split("-").map(Number);
+            return inicio < fin; // Retorna true si el primer número es menor que el segundo
+          },
+        );
+      schemaFields.gender = yusString().required(t("forms.required"));
+      break;
+    case "create-team":
+      schemaFields.name = yusString().required(t("forms.required"));
+      schemaFields.category_id = yup.number().required(t("forms.required"));
+      schemaFields.tournament_id = yup.number().required(t("forms.required"));
+      schemaFields.president_name = yusString().required(t("forms.required"));
+      schemaFields.coach_name = yusString().required(t("forms.required"));
+      schemaFields.phone = yusString()
+        .matches(/^\d{10}$/, "Número de teléfono no es válido")
+        .required();
+      schemaFields.email = yusString().email();
+      schemaFields.address = yusString();
+      schemaFields.image = yup.array().nullable();
+      schemaFields.location_id = yup.number().nullable();
+      break;
+    case "edit-user":
+      schemaFields.name = yusString().required(t("forms.required"));
+      schemaFields.email = yup
+        .string()
+        .email("Correo electrónico no válido")
+        .required(t("forms.required"));
+      // schemaFields.city = yup.string().required(t("forms.required"));
+      schemaFields.phone = yusString();
+      break;
+    default:
+      schemaFields = yup.mixed();
+  }
+  return yup.object().shape(schemaFields);
 }
