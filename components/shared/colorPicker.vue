19afe1edaecc88b438bab45b780f20a3b60230f9
Jose Terrazas <jozaguts@gmail.com>
2024-08-27 13:43:27 -0600

    feat(componentes): custom picker color added

---
+++ components/shared/colorPicker.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+interface Props {
+  show: boolean;
+}
+const { show } = withDefaults(defineProps<Props>(), { show: false });
+const emits = defineEmits(["update-value"]);
+const colorHandler = (color: string | boolean) => {
+  if (typeof color === "string") {
+    emits("update-value", color);
+  }
+};
+</script>
+<template>
+  <v-dialog
+    activator="parent"
+    max-width="264"
+    :scrim="false"
+    class="color-picker-dialog"
+    elevation="15"
+  >
+    <v-card
+      height="350"
+      width="264"
+      class="d-flex justify-center align-center flex-row pa-4 position-relative"
+    >
+      <v-color-picker
+        width="230"
+        elevation="0"
+        position="absolute"
+        mode="hexa"
+        @update:modelValue="colorHandler"
+      ></v-color-picker>
+    </v-card>
+    <nuxt-icon name="polygon" filled class="polygon"></nuxt-icon>
+  </v-dialog>
+</template>
+<style lang="sass">
+.color-picker-dialog, .v-color-picker-canvas
+  border-radius: var(--radius-md, 8px)
+.v-color-picker-preview__dot
+  display: none
+.v-color-picker__controls
+  padding: 1rem .5rem
+.v-color-picker-preview__sliders
+  padding: 0
+.nuxt-icon.polygon > svg
+  width: 66px
+  height: 44px
+.nuxt-icon.polygon
+  position: absolute
+  bottom: 0
+  left: 50%
+  transform: translate(-50%, 100%)
+.v-color-picker-edit__input > input
+  font-size: .8rem
+</style>
