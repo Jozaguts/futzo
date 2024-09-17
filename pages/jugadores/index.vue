adaffd1a9ce76b1cb9cc5e6f4ee9c3ace8e06d0c
Jose Terrazas <jozaguts@gmail.com>
2024-09-17 00:11:38 -0600

    feat(layout): page player

---
+++ pages/jugadores/index.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import PageLayout from "~/components/shared/page-layout/index.vue";
+import AppBar from "~/components/layout/app-bar.vue";
+import PlayersNavbarButtons from "~/components/pages/jugadores/players-navbar-buttons.vue";
+import NoPlayers from "~/components/pages/jugadores/no-players.vue";
+import PlayersDialog from "~/components/pages/jugadores/dialog/index.vue";
+</script>
+<template>
+  <PageLayout>
+    <template #app-bar>
+      <AppBar>
+        <template #buttons>
+          <PlayersNavbarButtons />
+        </template>
+      </AppBar>
+    </template>
+    <template #default>
+      <NoPlayers />
+      <!--      &lt;!&ndash;      <PlayersTable />&ndash;&gt;-->
+      <PlayersDialog />
+    </template>
+  </PageLayout>
+</template>
