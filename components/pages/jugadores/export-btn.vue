25d3c3a633f7fd3e61483f438ab875f30df78fe7
Jose Terrazas <jozaguts@gmail.com>
2024-10-05 13:03:18 -0600

    feat(players): import and export btns added

---
+++ components/pages/jugadores/export-btn.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+import AppBarSecondaryBtn from "~/components/shared/app-bar-secondary-btn.vue";
+</script>
+<template>
+  <AppBarSecondaryBtn text="Export .csv" class="mr-4" />
+</template>
--- components/pages/jugadores/players-navbar-buttons.vue
+++ components/pages/jugadores/players-navbar-buttons.vue
@@ -1,8 +1,12 @@
 <script lang="ts" setup>
 import CATBtn from "@/components/pages/jugadores/app-bar-cat-btn.vue";
 import SearchInput from "@/components/pages/jugadores/app-bar-search-input.vue";
+import ImportBtn from "@/components/pages/jugadores/import-btn.vue";
+import ExportBtn from "@/components/pages/jugadores/export-btn.vue";
 </script>
 <template>
   <SearchInput />
+  <ImportBtn />
+  <ExportBtn />
   <CATBtn />
 </template>
