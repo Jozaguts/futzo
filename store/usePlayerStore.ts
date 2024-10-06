f04105b620040ca2ec2002994b4e94a8ff24ff7a
Jose Terrazas <jozaguts@gmail.com>
2024-10-06 12:46:45 -0600

    feat(methods): update and created added

--- store/usePlayerStore.ts
+++ store/usePlayerStore.ts
@@ -1,5 +1,6 @@
 import { defineStore } from "pinia";
 import type { FormSteps, Player, PlayerStoreRequest } from "~/models/player";
+import { toast } from "vuetify-sonner";
 
 export const usePlayerStore = defineStore("playerStore", () => {
   const players = ref<Player[]>([]);
@@ -7,15 +8,50 @@ export const usePlayerStore = defineStore("playerStore", () => {
   const search = ref<string>("");
   const isEdition = ref<boolean>(false);
   const noPlayers = computed(() => players.value.length === 0);
-  const playerStoreRequest = ref<Partial<PlayerStoreRequest>>(
-    {} as PlayerStoreRequest,
-  );
-  const updatePlayer = async () => {};
-  const createPlayer = async () => {};
+  const playerStoreRequest = ref({} as PlayerStoreRequest);
+  const updatePlayer = async (id: number) => {
+    console.log(id);
+  };
+  const createPlayer = async () => {
+    const form = prepareForm();
+    const client = useSanctumClient();
+    await client("/api/v1/admin/players", {
+      method: "POST",
+      body: form,
+    })
+      .then(async () => {
+        toast.success("Jugador creado");
+        dialog.value = false;
+      })
+      .catch((error) => {
+        console.error(error.data?.errors);
+
+        toast.error(error.data?.message ?? "Error al crear Jugador");
+      });
+  };
   const steps = ref<FormSteps>({
-    current: "details-info",
+    current: "basic-info",
     completed: [],
   });
+  const prepareForm = (): FormData => {
+    const form = new FormData();
+    const appendData = (prefix: string, data: any) => {
+      for (const key in data) {
+        if (data[key] instanceof File) {
+          form.append(`${prefix}[${key}]`, data[key]);
+        } else {
+          form.append(`${prefix}[${key}]`, data[key]);
+        }
+      }
+    };
+
+    for (const key in playerStoreRequest.value) {
+      const data = playerStoreRequest.value[key];
+      appendData(key, data);
+    }
+
+    return form;
+  };
   return {
     players,
     dialog,
