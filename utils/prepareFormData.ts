ad10b2ab838809f5e67a5e7a0eab9018a1cf340a
Jose Terrazas <jozaguts@gmail.com>
2024-10-14 00:25:03 -0600

    refactor(prepareFormData): object added

--- utils/prepareFormData.ts
+++ utils/prepareFormData.ts
@@ -1,3 +1,5 @@
+import { isProxy } from "@vue/reactivity";
+
 const prepareForm = (requestData: Ref): FormData => {
   const form = new FormData();
   const appendData = (prefix: string, data: any) => {
@@ -7,6 +9,8 @@ const prepareForm = (requestData: Ref): FormData => {
       } else if (data[key] instanceof Date) {
         const date = data[key].toISOString().split("T")[0];
         form.append(`${prefix}[${key}]`, date);
+      } else if (isProxy(data[key])) {
+        form.append(`${prefix}[${key}]`, JSON.stringify(data[key]));
       } else {
         form.append(`${prefix}[${key}]`, data[key]);
       }
