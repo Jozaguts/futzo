8cc8822a424f222eb9439ce208ef66e1050c3e26
Jose Terrazas <jozaguts@gmail.com>
2024-11-10 10:32:47 -0600

    refactor(Player): getPlayers

--- store/usePlayerStore.ts
+++ store/usePlayerStore.ts
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import type { FormSteps, Player, PlayerStoreRequest } from "~/models/player";
+import type { FormSteps, Player, PlayerStoreRequest } from "~/models/Player";
 import { toast } from "vuetify-sonner";
 import prepareForm from "~/utils/prepareFormData";
 
@@ -10,6 +10,13 @@ export const usePlayerStore = defineStore("playerStore", () => {
   const isEdition = ref<boolean>(false);
   const noPlayers = computed(() => players.value.length === 0);
   const playerStoreRequest = ref({} as PlayerStoreRequest);
+  const playerId = ref(null);
+  const pagination = ref({
+    page: 1,
+    perPage: 10,
+    total: 0,
+    to: 1,
+  });
   const updatePlayer = async (id: number) => {
     console.log(id);
   };
@@ -23,12 +30,26 @@ export const usePlayerStore = defineStore("playerStore", () => {
       .then(async () => {
         toast.success("Jugador creado");
         dialog.value = false;
+        await getPlayers();
       })
       .catch((error) => {
         console.error(error.data?.errors);
 
         toast.error(error.data?.message ?? "Error al crear Jugador");
       });
+  };
+  const getPlayers = async () => {
+    try {
+      const client = useSanctumClient();
+      const response = await client(
+        `/api/v1/admin/players?per_page=${pagination.value.perPage}&page=${pagination.value.to}`,
+      );
+      pagination.value.total = response.meta.last_page;
+      pagination.value.page = response.meta.current_page;
+      players.value = response.data;
+    } catch (error) {
+      console.log(error);
+    }
   };
   const steps = ref<FormSteps>({
     current: "basic-info",
@@ -51,7 +72,10 @@ export const usePlayerStore = defineStore("playerStore", () => {
     isEdition,
     steps,
     playerStoreRequest,
+    playerId,
+    pagination,
     updatePlayer,
     createPlayer,
+    getPlayers,
   };
 });
