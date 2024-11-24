c7495a16d019a2c5730d7ff29c9ca0d10f46be43
Jose Terrazas <jozaguts@gmail.com>
2024-11-24 14:46:21 -0600

    refactor(plugin): custom-icon removed

--- plugins/custom-icons.ts
+++
@@ -1,9 +0,0 @@
-import { icons } from "~/nuxt-icon-set/svg";
-
-// import { icons } from '~/nuxt-icon-set/css'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.hook("vuetify:before-create", ({ vuetifyOptions }) => {
-    vuetifyOptions.icons = icons;
-  });
-});
