a379593aa79c33a2021348ce767fa365a4a41a7e
Jose Terrazas <jozaguts@gmail.com>
2025-01-26 21:09:13 -0600

    refactor(form): 01 general

--- components/pages/torneos/calendario/stepper/01-general.vue
+++ components/pages/torneos/calendario/stepper/01-general.vue
@@ -1,38 +1,30 @@
 <script lang="ts" setup>
+import SelectLocation from "~/components/pages/torneos/calendario/SelectLocation.vue";
 import useSchemas from "~/composables/useSchemas";
-import Calendar from "~/components/pages/torneos/calendar.vue";
 import {useTournamentStore} from "~/store";
-import AvailableLocations from "~/components/pages/torneos/calendario/available-locations.vue";
 
 const [parent] = useAutoAnimate();
-const {tournamentLocations, selectedLocations} =
-    storeToRefs(useTournamentStore());
+const {tournament, scheduleSettings} = storeToRefs(useTournamentStore());
 const {handleSubmit, resetForm, fields, validate, setValues, meta} =
     useSchemas("create-calendar");
-const datesModel = ref<Date[]>([]);
+const startDate = ref<Date>();
 const formatDate = (date: string) => {
   const [year, month, day] = date.split("-");
   return new Date(Number(year), Number(month) - 1, Number(day));
 };
 onMounted(async () => {
-  useTournamentStore()
-      .settingsSchedule()
-      .then((data) => {
-        const startDate = formatDate(data.start_date);
-        const endDate = formatDate(data.end_date);
-        datesModel.value.push(startDate);
-        datesModel.value.push(endDate);
-        resetForm({
-          values: {
-            start_date: startDate,
-            end_date: endDate,
-            game_time: data.game_time,
-            time_between_games: data.time_between_games,
-          },
-        });
-        tournamentLocations.value = data.locations;
-      });
+  startDate.value = formatDate(scheduleSettings.value?.start_date as Date);
+  resetForm({
+    values: {
+      start_date: startDate.value,
+      game_time: scheduleSettings.value?.game_time,
+      time_between_games: scheduleSettings.value?.time_between_games,
+    }
+  });
+
 });
+
+
 defineExpose({
   validate,
   handleSubmit,
@@ -42,27 +34,57 @@ defineExpose({
   <v-container class="container">
     <v-row>
       <v-col cols="12" lg="4" md="4">
-        <span class="text-body-1"> Fechas del torneo* </span>
+        <span class="text-body-1"> Nombre del torneo </span>
       </v-col>
       <v-col cols="12" lg="8" md="8">
-        <Calendar
-            v-model:dates="datesModel"
-            :multi-calendar="true"
-            @update:dates="
-            (dates: Date[]) => {
-              const start = dates ? dates[0] : null;
-              const end = dates ? dates[1] : null;
-              fields.start_date.fieldValue = start;
-              fields.end_date.fieldValue = end;
-            }
-          "
-            :position-values="{
-            top: -45,
-            left: 193,
-            transform: 'translate(0)',
-          }"
+        <v-text-field
+            density="compact"
+            :value="tournament?.name"
+            disabled
+            variant="outlined"
         />
-
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" lg="4" md="4">
+        <span class="text-body-1">Formato del torneo </span>
+      </v-col>
+      <v-col cols="12" lg="8" md="8">
+        <v-text-field
+            density="compact"
+            :value="scheduleSettings?.format?.name "
+            disabled
+            variant="outlined"
+            persistent-hint
+            :hint="scheduleSettings?.format?.description"
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" lg="4" md="4">
+        <span class="text-body-1">Estilo</span>
+      </v-col>
+      <v-col cols="12" lg="8" md="8">
+        <v-text-field
+            density="compact"
+            :value="scheduleSettings?.footballType?.name"
+            disabled
+            variant="outlined"
+            persistent-hint
+            :hint="scheduleSettings?.footballType?.description"
+        />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12" lg="4" md="4">
+        <span class="text-body-1"> Fecha de inicio del torneo* </span>
+      </v-col>
+      <v-col cols="12" lg="8" md="8" ref="parent">
+        <BaseCalendarInput
+            v-if="startDate"
+            v-model:start_date="startDate"
+            :multiCalendar="false"
+        />
         <div ref="parent">
           <small
               v-if="fields.start_date.fieldPropsValue['error-messages'][0]"
@@ -79,7 +101,7 @@ defineExpose({
       <v-col cols="12" lg="8" md="8">
         <v-text-field
             type="number"
-            outlined
+            variant="outlined"
             density="compact"
             v-model="fields.game_time.fieldValue"
             v-bind="fields.game_time.fieldPropsValue"
@@ -94,7 +116,7 @@ defineExpose({
       <v-col cols="12" lg="8" md="8">
         <v-text-field
             type="number"
-            outlined
+            variant="outlined"
             density="compact"
             v-model="fields.time_between_games.fieldValue"
             v-bind="fields.time_between_games.fieldPropsValue"
@@ -103,65 +125,14 @@ defineExpose({
       </v-col>
     </v-row>
     <v-row>
-      <v-col cols="12">
-        <span class="text-body-1">
-          Selecciona las ubicaciones para el torneo</span
-        >
+      <v-col cols="12" lg="4" md="4">
+        <span class="text-body-1"> Ubicaciones* </span>
       </v-col>
-      <v-col cols="12">
-        <available-locations
-            :locations="tournamentLocations"
-            v-model:selectedLocations="selectedLocations"
-        />
+      <v-col cols="12" lg="8" md="8">
+        <SelectLocation></SelectLocation>
       </v-col>
     </v-row>
-    <!--    <v-row v-if="locationSelected.length">-->
-    <!--      <v-col cols="12">-->
-    <!--        <v-tabs :mandatory="true" v-model="tab" show-arrows ref="parent2">-->
-    <!--          <v-tab-->
-    <!--            v-for="location in locationSelected"-->
-    <!--            :key="location.id"-->
-    <!--            :value="location.id"-->
-    <!--          >-->
-    <!--            {{ location.name }}-->
-    <!--          </v-tab>-->
-    <!--        </v-tabs>-->
-    <!--      </v-col>-->
-    <!--      <v-col>-->
-    <!--        <transition-expand mode="in-out" :duration="{ enter: 800, leave: 500 }">-->
-    <!--          <v-tabs-window v-model="tab">-->
-    <!--            <v-tabs-window-item-->
-    <!--              v-for="n in locationSelected"-->
-    <!--              :key="n.id"-->
-    <!--              :value="n.id"-->
-    <!--            >-->
-    <!--              <v-container>-->
-    <!--                <v-row>-->
-    <!--                  <v-col cols="12">-->
-    <!--                    <span class="text-body-1"> Horarios* </span>-->
-    <!--                  </v-col>-->
-    <!--                  <v-col cols="12">-->
-    <!--                    <v-row-->
-    <!--                      v-for="schedule in schedules_available"-->
-    <!--                      :key="schedule.day"-->
-    <!--                    >-->
-    <!--                      <v-col cols="4" lg="4" md="4">-->
-    <!--                        <v-switch :label="schedule.label" />-->
-    <!--                      </v-col>-->
-    <!--                      <v-col cols="8" align-self="center">-->
-    <!--                        <TimePicker-->
-    <!--                          v-model:from="schedule.hours.from"-->
-    <!--                          v-model:to="schedule.hours.to"-->
-    <!--                        />-->
-    <!--                      </v-col>-->
-    <!--                    </v-row>-->
-    <!--                  </v-col>-->
-    <!--                </v-row>-->
-    <!--              </v-container>-->
-    <!--            </v-tabs-window-item>-->
-    <!--          </v-tabs-window>-->
-    <!--        </transition-expand>-->
-    <!--      </v-col>-->
-    <!--    </v-row>-->
+
   </v-container>
 </template>
+
