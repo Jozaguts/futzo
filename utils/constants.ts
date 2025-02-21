473fd8d3873363f1b1d408a260cbc5932160cd59
Jose Terrazas <jozaguts@gmail.com>
2025-02-21 17:58:55 -0600

    feat(google): types

--- utils/constants.ts
+++ utils/constants.ts
@@ -4,3 +4,18 @@ export const FUTBOL_11_ID = 1;
 export const MAX_SIZE = 2.0;
 export const MIN_TEAMS = 8;
 export const MAX_TEAMS = 32;
+export const GOOGLE_MAPS_OPTIONS: google.maps.MapOptions = {
+    colorScheme: 'LIGHT',
+    center: {lat: 16.863266249728003, lng: -99.88556151300733},
+    zoom: 15,
+    mapTypeId: 'roadmap',
+    zoomControl: true,
+    cameraControl: false,
+    mapTypeControl: false,
+    scaleControl: false,
+    streetViewControl: false,
+    rotateControl: false,
+    fullscreenControl: false,
+    gestureHandling: "cooperative",
+    renderingType: 'VECTOR'
+}
