72d08ed2923d68c49992a62157caeef8aee65891
Jose Terrazas <jozaguts@gmail.com>
2025-02-21 18:00:01 -0600

    feat(LocationCard):google maps added

--- components/pages/ubicaciones/stepper/LocationStep.vue
+++ components/pages/ubicaciones/stepper/LocationStep.vue
@@ -7,6 +7,10 @@ import type {Prediction} from "~/interfaces";
 import search from "~/utils/googleSearch";
 import {useLocationStore} from "~/store";
 import type {AutocompletePrediction} from "~/models/Schedule";
+import {GOOGLE_MAPS_OPTIONS} from "~/utils/constants";
+import {storeToRefs} from "pinia";
+import {useForm} from "vee-validate";
+import {toTypedSchema} from "@vee-validate/yup";
 
 const {locationStoreRequest, isEdition} = storeToRefs(useLocationStore())
 const {defineField, errors, handleSubmit, validate} = useForm<LocationStoreRequest>({
@@ -18,6 +22,10 @@ const {defineField, errors, handleSubmit, validate} = useForm<LocationStoreReque
         autocomplete_prediction: object().required('El campo es requerido').default(locationStoreRequest.value),
         fields_count: number().required('La cantidad de campos de juego es requerida').default(locationStoreRequest.value.fields_count),
         tags: array().of(string()),
+        position: object({
+          lat: number().required(),
+          lng: number().required(),
+        }).default(locationStoreRequest.value.position)
       })
   ),
 })
@@ -28,7 +36,11 @@ const [address] = reactive(defineField('address'))
 const [tags] = reactive(defineField('tags'))
 const [fields_count] = reactive(defineField('fields_count'))
 const [autocomplete_prediction] = reactive(defineField('autocomplete_prediction'))
+const [position] = reactive(defineField('position'))
 let foundedLocations = ref([] as Location[])
+const mapElement = ref<HTMLElement>()
+let mapInstance = ref<google.maps.Map>()
+const marker = ref<google.maps.Marker>()
 const itemProps = (item: Prediction) => {
   return {
     title: item?.structured_formatting?.main_text,
@@ -41,14 +53,48 @@ const searchHandler = async (place: string) => {
     foundedLocations.value = response
   }
 }
-onMounted(() => {
-  if (isEdition.value) {
-    const {toUpdate} = useLocationStore()
-    name.value = toUpdate?.name as unknown as string
-    address.value = toUpdate?.address as unknown as string
-    city.value = toUpdate?.city as unknown as string
+onMounted(async () => {
+  if (window.google && window.google.maps) {
+    mapInstance.value = new window.google.maps.Map(mapElement.value, GOOGLE_MAPS_OPTIONS)
+    marker.value = new window.google.maps.Marker({
+      position: position.value,
+      map: mapInstance.value,
+      draggable: true,
+    })
+    updateMarker()
   }
 })
+const updateMarker = () => {
+  if (marker.value) {
+    marker.value.addListener("dragend", (event) => {
+      const newLat = event.latLng.lat();
+      const newLng = event.latLng.lng();
+      position.value = {lat: newLat, lng: newLng}
+      const geocoder = new window.google.maps.Geocoder()
+      geocoder.geocode({location: position.value}, (results, status) => {
+        if (status === 'OK') {
+          if (results[0]) {
+            address.value = results[0].formatted_address
+            let foundCity = ''
+            results[0].address_components.forEach((component) => {
+              if (component.types.includes('locality')) {
+                foundCity = component.long_name
+              }
+            })
+            city.value = foundCity
+
+            console.log('Nueva dirección:', address.value)
+            console.log('Nueva ciudad:', city.value)
+          } else {
+            console.error('No se encontraron resultados de geocodificación.')
+          }
+        } else {
+          console.error('Error en la geocodificación inversa:', status)
+        }
+      })
+    });
+  }
+}
 const valueHandler = (type: string, value: string) => {
   if (type === 'name') {
     locationStoreRequest.value.name = value
@@ -60,22 +106,31 @@ defineExpose({
   tags
 });
 const updateValue = (value: AutocompletePrediction) => {
+  if (value.place_id) {
+    const placesService = new window.google.maps.places.PlacesService(mapInstance.value);
+    placesService.getDetails({placeId: value.place_id}, (place, status) => {
+      if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+        mapInstance.value.setCenter(place.geometry.location);
+        marker.value.setPosition(place.geometry.location);
+      } else {
+        console.error("Error al obtener detalles del lugar:", status);
+      }
+    });
+  }
   autocomplete_prediction.value = value
   city.value = value.structured_formatting?.secondary_text
   address.value = value?.description
   locationStoreRequest.value.city = value.structured_formatting?.secondary_text
   locationStoreRequest.value.address = value?.description
 }
+
 </script>
 <template>
   <v-container class="pa-0">
     <v-row>
-      <v-col cols="12" lg="4" md="4">
-        <span class="text-body-1"/>
-      </v-col>
-      <v-col cols="12" lg="8" md="8">
+      <v-col cols="12">
         <v-autocomplete
-            label="Buscar ubicación"
+            label="Direccion del centro deportivo o canchas de juego"
             v-model="searchString"
             :items="foundedLocations"
             outlined
@@ -90,6 +145,9 @@ const updateValue = (value: AutocompletePrediction) => {
             :error-messages="errors.city"
         >
         </v-autocomplete>
+      </v-col>
+      <v-col cols="12">
+        <div id="map" ref="mapElement" class="futzo-rounded"/>
       </v-col>
     </v-row>
     <v-row>
@@ -122,7 +180,7 @@ const updateValue = (value: AutocompletePrediction) => {
     </v-row>
     <v-row>
       <v-col cols="12" lg="4" md="4">
-        <span class="text-body-1">Nombre*</span>
+        <span class="text-body-1">Nombre del centro deportivo / canchas de juego*</span>
       </v-col>
       <v-col cols="12" lg="8" md="8">
         <v-text-field
@@ -153,3 +211,11 @@ const updateValue = (value: AutocompletePrediction) => {
     </v-row>
   </v-container>
 </template>
+<style>
+#map {
+  height: 250px;
+  width: 100%;
+
+  margin: 0 auto;
+}
+</style>
