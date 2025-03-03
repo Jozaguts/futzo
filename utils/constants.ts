22bfb04c12d32df07422408d160c84625a81d136
Jose Terrazas <jozaguts@gmail.com>
2025-03-02 20:30:33 -0600

    refactor(const): DEFAULT_POSITION,  GOOGLE_MAPS_OPTIONS and DEFAULT_AVAILABILITY_HOURS added

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,12 +1,15 @@
+import type {LocationAvailability} from "~/models/Location";
+
 export const MAIN_PADDING_TOP = 48;
 export const MAIN_PADDING_BOTTOM = 64;
 export const FUTBOL_11_ID = 1;
 export const MAX_SIZE = 2.0;
 export const MIN_TEAMS = 8;
 export const MAX_TEAMS = 32;
-export const GOOGLE_MAPS_OPTIONS: google.maps.MapOptions = {
+export const DEFAULT_POSITION = {lat: 16.8639515, lng: -99.8822807};
+export const GOOGLE_MAPS_OPTIONS = {
     colorScheme: 'LIGHT',
-    center: {lat: 16.863266249728003, lng: -99.88556151300733},
+    center: DEFAULT_POSITION,
     zoom: 15,
     mapTypeId: 'roadmap',
     zoomControl: true,
@@ -19,3 +22,17 @@ export const GOOGLE_MAPS_OPTIONS: google.maps.MapOptions = {
     gestureHandling: "cooperative",
     renderingType: 'VECTOR'
 }
+export const DEFAULT_AVAILABILITY_HOURS: LocationAvailability[] = [
+    {
+        id: 1,
+        name: 'Campo 1',
+        isCompleted: false,
+        monday: {enabled: false, start: {hours: '09', minutes: '00'}, end: {hours: '17', minutes: '00'}},
+        tuesday: {enabled: false, start: {hours: '09', minutes: '00'}, end: {hours: '17', minutes: '00'}},
+        wednesday: {enabled: false, start: {hours: '09', minutes: '00'}, end: {hours: '17', minutes: '00'}},
+        thursday: {enabled: false, start: {hours: '09', minutes: '00'}, end: {hours: '17', minutes: '00'}},
+        friday: {enabled: true, start: {hours: '09', minutes: '00'}, end: {hours: '17', minutes: '00'}},
+        saturday: {enabled: true, start: {hours: '09', minutes: '00'}, end: {hours: '17', minutes: '00'}},
+        sunday: {enabled: true, start: {hours: '09', minutes: '00'}, end: {hours: '17', minutes: '00'}},
+    }
+]
