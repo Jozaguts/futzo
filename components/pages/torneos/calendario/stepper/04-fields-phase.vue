4040f571f3ea649f7874574ebf98e73a9b924edf
Jose Terrazas <jozaguts@gmail.com>
2025-03-13 14:14:02 -0600

    feat(tournament) nextHandler added

--- components/pages/torneos/calendario/stepper/04-fields-phase.vue
+++ components/pages/torneos/calendario/stepper/04-fields-phase.vue
@@ -3,11 +3,10 @@ import {useTournamentStore} from "~/store";
 import LocationFormStep from '~/components/pages/torneos/calendario/location-form-step.vue'
 
 const {tournamentId, scheduleStoreRequest} = storeToRefs(useTournamentStore())
-// todo los campos que me debo de trare es por cada locaicon entonce sis eligieron 3 locaciones, y cada campo tiene 3 campos tendria que ser 9 campo por organizar
 
-const {fields: form, meta} = useSchemas('calendar-location-step', {
+const {meta, validate} = useSchemas('calendar-location-step', {
   tournament_id: tournamentId.value,
-  availability: scheduleStoreRequest.value?.general?.locations.map((location) => ({id: location.id, name: location.name})),
+  availability: scheduleStoreRequest.value.fields_phase
 })
 
 
@@ -16,23 +15,22 @@ const isValid = computed(() => {
 })
 defineExpose({
   isValid,
+  validate
 })
 
 const currentStep = ref()
 const fields = ref()
 
-onMounted(async () => {
-  const locationIds = scheduleStoreRequest.value.general.locations.map(location => location.id)
-  const client = useSanctumClient()
-  fields.value = await client(`/api/v1/admin/locations/fields?location_ids=${locationIds.join(',')}`)
-  console.log('step 4 mounted', fields.value)
-  currentStep.value = fields.value[0]?.step
-})
 const getStepAttribute = (attribute: string, step: number) => fields.value.filter(field => field.step === step)[0][attribute]
 const updateChangedHandler = (data) => {
   console.log({data})
 }
-const nextHandler = () => {
+const nextHandler = (value) => {
+  scheduleStoreRequest.value.fields_phase.map((field) => {
+    if (field.field_id === value.field_id) {
+      field.availability.isCompleted = true;
+    }
+  })
   if (currentStep.value < fields.value.length) {
     currentStep.value += 1
   }
@@ -65,7 +63,7 @@ const backHandler = () => {
             </p>
           </template>
           <template #subtitle="item">
-            <p class="tex-body-2 text-capitalize"> {{ getStepAttribute('name', item.step) }}</p>
+            <p class="tex-body-2 text-capitalize"> {{ getStepAttribute('field_name', item.step) }}</p>
           </template>
           <template v-for="field in fields" :key="field.step" #[`item.${field.step}`]="item">
             <LocationFormStep
