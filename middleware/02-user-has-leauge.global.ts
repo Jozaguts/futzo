ddf03bbaa059dbda60c880d9d8093f058cc849f5
Jose Terrazas <jozaguts@gmail.com>
2025-04-13 22:25:53 -0600

    fix(middleware) checking phone value

--- middleware/02-user-has-leauge.global.ts
+++ middleware/02-user-has-leauge.global.ts
@@ -1,15 +1,14 @@
-import type { User } from "~/models/user";
+import type {User} from "~/models/user";
 
 export default defineNuxtRouteMiddleware((to) => {
-  if (import.meta.server) return;
-  const user = useSanctumUser<User>();
-  const isLogin = !!user.value?.email;
-  // skip middleware on server
+    if (import.meta.server) return;
+    const user = useSanctumUser<User>();
+    const isLogin = !!user.value?.email || !!user.value?.phone;
 
-  if (user.value?.roles.includes("super administrador")) {
-    return;
-  }
-  if (isLogin && to.path !== "/bienvenido" && !user.value?.has_league) {
-    return navigateTo({ name: "bienvenido" });
-  }
+    if (user.value?.roles.includes("super administrador")) {
+        return;
+    }
+    if (isLogin && to.path !== "/bienvenido" && !user.value?.has_league) {
+        return navigateTo({name: "bienvenido"});
+    }
 });
