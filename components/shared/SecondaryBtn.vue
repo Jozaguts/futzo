a813c5decb14c566674ed3f4244318c8a32b3669
Jose Terrazas <jozaguts@gmail.com>
2025-04-26 13:36:59 -0600

    feat(button): enhance secondary button styling and structure

--- components/shared/SecondaryBtn.vue
+++ components/shared/SecondaryBtn.vue
@@ -6,9 +6,14 @@ const emits = defineEmits(["btn-click"]);
 </script>
 <template>
   <v-btn
+      border="solid #d0d5dd"
       class="app-bar-secondary-btn"
       size="large"
+      variant="outlined"
       @click="() => emits('btn-click')"
-  >{{ text }}
+  >
+    <span class="app-bar-secondary-btn__input_text">
+      {{ text }}
+    </span>
   </v-btn>
 </template>
