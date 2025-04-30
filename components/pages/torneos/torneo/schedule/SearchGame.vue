1a866a5906e0881b708b2596d96ca8d82a8b7b00
Jose Terrazas <jozaguts@gmail.com>
2025-04-30 16:18:18 -0600

    feat(search): implement debounced search functionality for tournament schedules

--- components/pages/torneos/torneo/schedule/SearchGame.vue
+++ components/pages/torneos/torneo/schedule/SearchGame.vue
@@ -1,4 +1,17 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import {useScheduleStore} from "~/store";
+
+const {getTournamentSchedules} = useScheduleStore()
+const {schedulePagination, schedules} = storeToRefs(useScheduleStore());
+
+const searchHandler = useDebounceFn((value: string) => {
+  schedules.value.rounds = [];
+  schedulePagination.value.search = value
+  schedulePagination.value.currentPage = 1
+  getTournamentSchedules()
+}, 500)
+
+</script>
 <template>
-  <v-text-field min-width="300" append-inner-icon="mdi-magnify" label="Buscar un partido..."></v-text-field>
+  <v-text-field v-model="schedulePagination.search" min-width="300" append-inner-icon="mdi-magnify" label="Buscar un equipo..." @update:model-value=" searchHandler"></v-text-field>
 </template>
