74649057308f278f851e6125656b74be20775cac
Jose Terrazas <jozaguts@gmail.com>
2025-05-19 13:49:54 -0600

    feat(header): update titles and subtitles for inscription page

--- components/pages/equipos/CreateTeamDialog/Header.vue
+++ components/pages/equipos/CreateTeamDialog/Header.vue
@@ -1,33 +1,44 @@
 <script lang="ts" setup>
-import { useTeamStore } from "~/store";
+  import { useTeamStore } from '~/store'
 
-const { dialog, steps, isEdition } = storeToRefs(useTeamStore());
-const title = computed(() => {
-  switch (steps.value.current) {
-    case "createTeam":
-      return isEdition.value ? "Editar equipo" : "Crear un equipo";
-    case "createDt":
-      return isEdition.value ? "Editar DT" : "Crear DT";
-    case "createOwner":
-      return isEdition.value ? "Editar dueño" : "Crear dueño";
-  }
-});
-const subtitle = computed(() => {
-  switch (steps.value.current) {
-    case "createTeam":
-      return isEdition.value
-        ? "Modifica los detalles del equipo."
-        : "Completa los detalles del equipo.";
-    case "createDt":
-      return isEdition.value
-        ? "Modifica los detalles del DT."
-        : "Completa los detalles del DT.";
-    case "createOwner":
-      return isEdition.value
-        ? "Modifica los detalles del dueño."
-        : "Completa los detalles del dueño.";
-  }
-});
+  const { dialog, steps, isEdition } = storeToRefs(useTeamStore())
+  const page = useRoute()
+  const isInscriptionPage = computed(
+    () => page.name === 'torneos-torneo-inscripcion'
+  )
+  const title = computed(() => {
+    console.log(page)
+    switch (steps.value.current) {
+      case 'createTeam':
+        return isEdition.value
+          ? `${isInscriptionPage.value ? 'Editar' : 'Editar'} equipo`
+          : `${isInscriptionPage.value ? 'Inscribir' : 'Crear'} equipo`
+      case 'createDt':
+        return isEdition.value
+          ? `${isInscriptionPage.value ? 'Editar' : 'Editar'} DT`
+          : `${isInscriptionPage.value ? 'Inscribir' : 'Crear'} DT`
+      case 'createOwner':
+        return isEdition.value
+          ? `${isInscriptionPage.value ? 'Editar' : 'Editar'} dueño`
+          : `${isInscriptionPage.value ? 'Inscribir' : 'Crear'} dueño`
+    }
+  })
+  const subtitle = computed(() => {
+    switch (steps.value.current) {
+      case 'createTeam':
+        return isEdition.value
+          ? 'Modifica los detalles del equipo.'
+          : 'Completa los detalles del equipo.'
+      case 'createDt':
+        return isEdition.value
+          ? 'Modifica los detalles del DT.'
+          : 'Completa los detalles del DT.'
+      case 'createOwner':
+        return isEdition.value
+          ? 'Modifica los detalles del dueño.'
+          : 'Completa los detalles del dueño.'
+    }
+  })
 </script>
 <template>
   <v-card-item>
