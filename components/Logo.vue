7f70c9eee38c6a9b644acd24cbe0257f957af2af
Jose Terrazas <jozaguts@gmail.com>
2025-05-19 17:49:17 -0600

    feat(logo): update logo handling and add navigation to home

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,30 +1,35 @@
 <script lang="ts" setup>
-import { useTheme } from "vuetify";
+  import { useTheme } from 'vuetify'
 
-const logo = ref("/futzo/logos/horizontal/logo-12.png");
+  const logo = ref('/futzo/logos/horizontal/logo-12.png')
 
-export interface Props {
-  maxWidth?: string | number;
-}
+  export interface Props {
+    maxWidth?: string | number
+  }
 
-const props = withDefaults(defineProps<Props>(), {
-  maxWidth: "100%",
-});
-watch(
-  useTheme().global.current,
-  (value) => {
-    value.dark
-      ? (logo.value = "/futzo/logos/horizontal/logo-14.png")
-      : (logo.value = "/futzo/logos/horizontal/logo-12.png");
-  },
-  { deep: true },
-);
+  const props = withDefaults(defineProps<Props>(), {
+    maxWidth: '100%',
+  })
+  watch(
+    useTheme().global.current,
+    (value) => {
+      value.dark
+        ? (logo.value = '/futzo/logos/horizontal/logo-14.png')
+        : (logo.value = '/futzo/logos/horizontal/logo-12.png')
+    },
+    { deep: true }
+  )
+  const goToHome = () => {
+    if (useRoute().name !== 'torneos-torneo-inscripcion') {
+      useRouter().push({ name: 'index' })
+    }
+  }
 </script>
 <template>
   <v-img
     :max-width="props.maxWidth"
     :src="logo"
     class="cursor-pointer"
-    @click="() => $router.push({ name: 'index' })"
+    @click="goToHome"
   ></v-img>
 </template>
