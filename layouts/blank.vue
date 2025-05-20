1790fa90cec3116c2958a2e154cf8eeec27d8a9e
Jose Terrazas <jozaguts@gmail.com>
2025-05-20 16:33:13 -0600

    refactor(blank): clean up template and script formatting

--- layouts/blank.vue
+++ layouts/blank.vue
@@ -1,15 +1,11 @@
 <template>
   <v-app app>
     <ClientOnly>
-      <VSonner position="top-right"  />
+      <VSonner position="top-right" />
     </ClientOnly>
-      <slot></slot>
-    <v-footer v-if="useGlobalStore().showFooter" absolute app style="max-width: 50%" color="background">
-     {{new Date().getFullYear()}} &copy; Futzo
-    </v-footer>
+    <slot />
   </v-app>
 </template>
 <script setup lang="ts">
-import {VSonner} from "vuetify-sonner";
-import {useGlobalStore} from "~/store";
+  import { VSonner } from 'vuetify-sonner'
 </script>
