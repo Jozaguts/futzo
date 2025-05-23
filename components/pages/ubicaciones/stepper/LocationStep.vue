683cf7071439dc6043767fef7c4d51449a85186a
Jose Terrazas <jozaguts@gmail.com>
2025-05-23 14:10:13 -0600

    refactor(components): improve code formatting and readability

--- components/pages/ubicaciones/stepper/LocationStep.vue
+++ components/pages/ubicaciones/stepper/LocationStep.vue
@@ -1,58 +1,70 @@
 <script lang="ts" setup>
-import {ref} from "vue";
-import type {LocationStoreRequest} from "~/models/Location";
-import {array, object, string, number} from "yup";
-import type {Prediction} from "~/interfaces";
+import {ref} from 'vue'
+import type {LocationStoreRequest} from '~/models/Location'
+import {array, object, string, number} from 'yup'
+import type {Prediction} from '~/interfaces'
 import {usePlaceSearch, getPlaceDetails} from '~/utils/googleSearch'
-import {useLocationStore} from "~/store";
-import type {AutocompletePrediction} from "~/models/Schedule";
-import {storeToRefs} from "pinia";
-import {useForm} from "vee-validate";
-import {toTypedSchema} from "@vee-validate/yup";
+import {useLocationStore} from '~/store'
+import type {AutocompletePrediction} from '~/models/Schedule'
+import {storeToRefs} from 'pinia'
+import {useForm} from 'vee-validate'
+import {toTypedSchema} from '@vee-validate/yup'
 import {GoogleMap, AdvancedMarker} from 'vue3-google-map'
 
 const {locationStoreRequest, isEdition} = storeToRefs(useLocationStore())
-const {defineField, errors, meta, controlledValues, setFieldError} = useForm<LocationStoreRequest>({
-  validationSchema: toTypedSchema(
-      object({
-        name: string().required('El campo es requerido').default(locationStoreRequest.value.name),
-        city: string().required('El campo es requerido').default(locationStoreRequest.value.city),
-        address: string().required('El campo es requerido').default(locationStoreRequest.value.address),
-        autocomplete_prediction: object().required('El campo es requerido').default(locationStoreRequest.value.autocomplete_prediction),
-        fields_count: number().required('La cantidad de campos de juego es requerida').default(locationStoreRequest.value.fields_count),
-        tags: array().of(string()),
-        position: object({
-          lat: number().required(),
-          lng: number().required(),
-        }).default(locationStoreRequest.value.position),
-      })
-  ),
-})
-const searchString = ref('');
+const {defineField, errors, meta, controlledValues, setFieldError} =
+    useForm<LocationStoreRequest>({
+      validationSchema: toTypedSchema(
+          object({
+            name: string()
+                .required('El campo es requerido')
+                .default(locationStoreRequest.value.name),
+            city: string()
+                .required('El campo es requerido')
+                .default(locationStoreRequest.value.city),
+            address: string()
+                .required('El campo es requerido')
+                .default(locationStoreRequest.value.address),
+            autocomplete_prediction: object()
+                .required('El campo es requerido')
+                .default(locationStoreRequest.value.autocomplete_prediction),
+            fields_count: number()
+                .required('La cantidad de campos de juego es requerida')
+                .default(locationStoreRequest.value.fields_count),
+            tags: array().of(string()),
+            position: object({
+              lat: number().required(),
+              lng: number().required(),
+            }).default(locationStoreRequest.value.position),
+          })
+      ),
+    })
+const searchString = ref('')
 const {search} = usePlaceSearch()
 const [name] = reactive(defineField('name'))
 const [city] = reactive(defineField('city'))
 const [address] = reactive(defineField('address'))
 const [tags] = reactive(defineField('tags'))
 const [fields_count] = reactive(defineField('fields_count'))
-const [autocomplete_prediction] = reactive(defineField('autocomplete_prediction'))
+const [autocomplete_prediction] = reactive(
+    defineField('autocomplete_prediction')
+)
 const [position] = reactive(defineField('position'))
 let foundedLocations = ref([] as AutocompletePrediction[])
 const tag = ref<string>('')
 const tagHandler = () => {
-
-  setFieldError('tags', 'La etiqueta ya existe o está vacía');
-  const trimmedTag = tag.value?.trim();
+  setFieldError('tags', 'La etiqueta ya existe o está vacía')
+  const trimmedTag = tag.value?.trim()
   if (!trimmedTag || tags.value?.includes(trimmedTag)) {
-    setFieldError('tags', 'La etiqueta ya existe o está vacía');
-    return;
+    setFieldError('tags', 'La etiqueta ya existe o está vacía')
+    return
   }
 
-  tags.value = [...(tags.value || []), trimmedTag];
-  tag.value = '';
+  tags.value = [...(tags.value || []), trimmedTag]
+  tag.value = ''
 }
 const removeTag = (tagToRemove: string) => {
-  tags.value = tags.value?.filter(tag => tag !== tagToRemove)
+  tags.value = tags.value?.filter((tag) => tag !== tagToRemove)
 }
 const itemProps = (item: Prediction) => {
   return {
@@ -68,28 +80,28 @@ const searchHandler = async (place: string) => {
   }
 }
 const updateValue = async (value: AutocompletePrediction) => {
-
   if (value?.place_id) {
-    const details = await getPlaceDetails(value.place_id);
+    const details = await getPlaceDetails(value.place_id)
     defineField('name', details?.name as string)
     locationStoreRequest.value.name = details?.name as string
     name.value = details?.name as string
     if (details?.lat && details?.lng) {
       locationStoreRequest.value.position = {
         lat: details.lat,
-        lng: details.lng
-      };
-      position.value = locationStoreRequest.value.position;
+        lng: details.lng,
+      }
+      position.value = locationStoreRequest.value.position
     }
   } else {
-    console.error('No se pudieron obtener coordenadas del lugar.');
+    console.error('No se pudieron obtener coordenadas del lugar.')
   }
-  autocomplete_prediction.value = value;
-  city.value = value.structured_formatting?.secondary_text;
-  address.value = value?.description;
-  locationStoreRequest.value.city = value.structured_formatting?.secondary_text;
-  locationStoreRequest.value.address = value?.description;
-};
+  autocomplete_prediction.value = value
+  city.value = value.structured_formatting?.secondary_text
+  address.value = value?.description
+  locationStoreRequest.value.city =
+      value.structured_formatting?.secondary_text
+  locationStoreRequest.value.address = value?.description
+}
 const isValidFrom = computed(() => meta.value.valid)
 const controlledValues2 = computed(() => controlledValues.value)
 const markerOptions = computed(() => {
@@ -104,14 +116,13 @@ watch(controlledValues2, (value) => {
 onMounted(async () => {
   if (isEdition.value) {
     locationStoreRequest.value.completed = true
-    if (locationStoreRequest.value.tags.length) {
+    if (locationStoreRequest.value?.tags?.length) {
       tags.value = [...locationStoreRequest.value.tags]
     }
   }
 })
 </script>
 <template>
-
   <v-container class="pa-0">
     <v-row>
       <v-col cols="12">
@@ -150,11 +161,7 @@ onMounted(async () => {
         <span class="text-body-1">Nombre</span>
       </v-col>
       <v-col cols="12" lg="8" md="8">
-        <v-text-field
-            :value="name"
-            readonly
-            :error-messages="errors.name"
-        >
+        <v-text-field :value="name" readonly :error-messages="errors.name">
         </v-text-field>
       </v-col>
     </v-row>
@@ -188,9 +195,7 @@ onMounted(async () => {
     </v-row>
 
     <v-row>
-      <v-col cols="12" lg="4" md="4">
-        Campos de juego
-      </v-col>
+      <v-col cols="12" lg="4" md="4"> Campos de juego</v-col>
       <v-col cols="12" lg="8" md="8">
         <v-number-input
             v-model="fields_count"
@@ -202,7 +207,8 @@ onMounted(async () => {
             :hideInput="false"
             inset
             :min="1"
-            variant="solo"></v-number-input>
+            variant="solo"
+        ></v-number-input>
       </v-col>
     </v-row>
     <v-row>
