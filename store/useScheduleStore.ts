1efcf0fbba04601e0435eed4c32c3b028436cfbd
Jose Terrazas <jozaguts@gmail.com>
2025-05-22 19:40:11 -0600

    feat(games): implement game show and update endpoints, add fields resource

--- store/useScheduleStore.ts
+++ store/useScheduleStore.ts
@@ -1,219 +1,304 @@
-import {defineStore} from 'pinia';
+import { defineStore } from 'pinia';
 import type {
-    EliminationPhase,
-    FootballType,
-    Format, FormEliminationPhaseStep,
-    FormGeneralScheduleRequest, FormLocationAvailabilityStep,
-    FormRegularPhaseStep,
-    RoundStatus, ScheduleRoundStatus,
-    ScheduleSettings,
-    ScheduleStoreRequest,
-    Tournament,
-    TournamentSchedule
-} from "~/models/Schedule";
-import {useTournamentStore} from "~/store/useTournamentStore";
-import type {Ref} from "vue";
-import type {IPagination} from "~/interfaces";
-import type {CalendarStepsForm} from "~/models/tournament";
+  EliminationPhase,
+  FootballType,
+  Format,
+  FormEliminationPhaseStep,
+  FormGeneralScheduleRequest,
+  FormLocationAvailabilityStep,
+  FormRegularPhaseStep,
+  RoundStatus,
+  ScheduleRoundStatus,
+  ScheduleSettings,
+  ScheduleStoreRequest,
+  Tournament,
+  TournamentSchedule,
+} from '~/models/Schedule';
+import { useTournamentStore } from '~/store/useTournamentStore';
+import type { Ref } from 'vue';
+import type { IPagination } from '~/interfaces';
+import type { CalendarStepsForm } from '~/models/tournament';
 
 export const useScheduleStore = defineStore('scheduleStore', () => {
-    const tournamentStore = useTournamentStore();
-    const scheduleDialog = ref(false);
-    const scheduleParams = ref<{ leagueId: number; tournamentId: number }>();
-    const daysToPlay = ref([
+  const tournamentStore = useTournamentStore();
+  const scheduleDialog = ref(false);
+  const scheduleParams = ref<{ leagueId: number; tournamentId: number }>();
+  const daysToPlay = ref([
+    {
+      days: [
+        'lunes',
+        'martes',
+        'miércoles',
+        'jueves',
+        'viernes',
+        'sábado',
+        'domingo',
+      ],
+      key: 'all-days',
+      text: 'Toda la semana',
+    },
+    {
+      days: ['viernes', 'sábado', 'domingo'],
+      key: 'weekend-days',
+      text: 'Viernes | Sábado | Domingo',
+    },
+    { days: ['sábado', 'domingo'], key: 'weekend', text: 'Sábado | Domingo' },
+    { days: ['domingo'], key: 'sunday', text: 'Domingo' },
+    { days: [], key: 'other', text: 'Otro' },
+  ]);
+  const daysToPlaySelected = ref();
+  const daysToPlayCustomSelected = ref();
+  const schedules = ref<TournamentSchedule>({
+    rounds: [],
+    tournament: {} as Tournament,
+  });
+  const noSchedules = computed(() => schedules.value?.rounds?.length === 0);
+  const isLoadingSchedules = ref(false);
+  const schedulePagination = ref<
+    IPagination & { filterBy?: RoundStatus | string; search?: string }
+  >({
+    currentPage: 1,
+    perPage: 10,
+    lastPage: 1,
+    total: 0,
+    sort: 'asc',
+    filterBy: undefined,
+    search: undefined,
+  });
+  const scheduleSettings = ref<ScheduleSettings>({
+    start_date: new Date(),
+    end_date: null,
+    round_trip: false,
+    elimination_round_trip: true,
+    game_time: 0,
+    min_teams: 0,
+    max_teams: 0,
+    time_between_games: 0,
+    teams: 0,
+    format: {} as Format,
+    footballType: {} as FootballType,
+    locations: [],
+    tiebreakers: [],
+    phases: [] as EliminationPhase[],
+  });
+  const scheduleStoreRequest = ref<ScheduleStoreRequest>({
+    general: {} as FormGeneralScheduleRequest,
+    regular_phase: {} as FormRegularPhaseStep,
+    elimination_phase: {} as FormEliminationPhaseStep,
+    fields_phase: [] as FormLocationAvailabilityStep[],
+  });
+  const calendarSteps = ref<CalendarStepsForm>({
+    current: 'general',
+    steps: [
+      {
+        step: 'general',
+        completed: false,
+        label: 'General',
+      },
+      {
+        step: 'regular',
+        completed: false,
+        label: 'Fase Regular',
+      },
+      {
+        step: 'elimination',
+        completed: false,
+        label: 'Fase de Eliminación',
+      },
+      {
+        step: 'fields',
+        completed: false,
+        label: 'Campos de juego',
+      },
+    ],
+  });
+
+  const scheduleRoundStatus = ref<ScheduleRoundStatus[]>([
+    { value: 'programado', text: 'Programada' },
+    { value: 'en_progreso', text: 'En progreso' },
+    { value: 'completado', text: 'Completada' },
+    { value: 'cancelado', text: 'Cancelada' },
+  ]);
+
+  const $resetScheduleStore = () => {
+    scheduleDialog.value = false;
+    scheduleParams.value = undefined;
+    daysToPlaySelected.value = undefined;
+    daysToPlayCustomSelected.value = undefined;
+    schedules.value = {
+      rounds: [],
+      tournament: {} as Tournament,
+    };
+    isLoadingSchedules.value = false;
+    schedulePagination.value = {
+      currentPage: 1,
+      perPage: 10,
+      lastPage: 1,
+      total: 0,
+      sort: 'asc',
+      filterBy: undefined,
+      search: undefined,
+    };
+    scheduleSettings.value = {
+      start_date: new Date(),
+      end_date: null,
+      round_trip: false,
+      elimination_round_trip: true,
+      game_time: 0,
+      min_teams: 0,
+      max_teams: 0,
+      time_between_games: 0,
+      teams: 0,
+      format: {} as Format,
+      footballType: {} as FootballType,
+      locations: [],
+      tiebreakers: [],
+      phases: [] as EliminationPhase[],
+    };
+    scheduleStoreRequest.value = {
+      general: {} as FormGeneralScheduleRequest,
+      regular_phase: {} as FormRegularPhaseStep,
+      elimination_phase: {} as FormEliminationPhaseStep,
+      fields_phase: [] as FormLocationAvailabilityStep[],
+    };
+    calendarSteps.value = {
+      current: 'general',
+      steps: [
         {
-            days: [
-                'lunes',
-                'martes',
-                'miércoles',
-                'jueves',
-                'viernes',
-                'sábado',
-                'domingo',
-            ],
-            key: 'all-days',
-            text: 'Toda la semana',
+          step: 'general',
+          completed: false,
+          label: 'General',
         },
         {
-            days: ['viernes', 'sábado', 'domingo'],
-            key: 'weekend-days',
-            text: 'Viernes | Sábado | Domingo',
+          step: 'regular',
+          completed: false,
+          label: 'Fase Regular',
         },
-        {days: ['sábado', 'domingo'], key: 'weekend', text: 'Sábado | Domingo'},
-        {days: ['domingo'], key: 'sunday', text: 'Domingo'},
-        {days: [], key: 'other', text: 'Otro'},
-    ]);
-    const daysToPlaySelected = ref();
-    const daysToPlayCustomSelected = ref();
-    const schedules = ref<TournamentSchedule>({
-            rounds: [],
-            tournament: {} as Tournament
-        }
+        {
+          step: 'elimination',
+          completed: false,
+          label: 'Fase de Eliminación',
+        },
+        {
+          step: 'fields',
+          completed: false,
+          label: 'Campos de juego',
+        },
+      ],
+    };
+  };
+  const getTournamentSchedules = async () => {
+    isLoadingSchedules.value = true;
+    const client = useSanctumClient();
+    let url = `/api/v1/admin/tournaments/${tournamentStore.tournamentId}/schedule?page=${schedulePagination.value.currentPage}`;
+
+    if (schedulePagination.value.filterBy) {
+      url += `&filterBy=${schedulePagination.value.filterBy}`;
+    }
+    if (schedulePagination.value.search) {
+      url += `&search=${schedulePagination.value.search}`;
+    }
+
+    const response = await client(url);
+    const newRounds = response.rounds ?? [];
+    if (!schedules.value.rounds.length) {
+      schedules.value.rounds = [];
+    }
+    schedules.value.tournament = response.tournament;
+    schedules.value.rounds.push(...newRounds);
+    schedulePagination.value.currentPage += 1;
+    schedulePagination.value.lastPage = response.pagination.total_rounds;
+    isLoadingSchedules.value = false;
+  };
+  const fetchSchedule = async () => {
+    isLoadingSchedules.value = true;
+    const client = useSanctumClient();
+    schedules.value = await client(
+      `/api/v1/admin/tournaments/${tournamentStore.tournamentId}/schedule?page=${schedulePagination.value.currentPage}`
+    ).finally(() => {
+      isLoadingSchedules.value = false;
+    });
+  };
+  const updateStatusGame = async (
+    roundId: number,
+    status: RoundStatus,
+    tournamentId: number
+  ) => {
+    const client = useSanctumClient();
+    await client(
+      `api/v1/admin/tournaments/${tournamentId}/schedule/rounds/${roundId}`,
+      {
+        method: 'PUT',
+        body: {
+          status,
+        },
+      }
     );
-    const noSchedules = computed(() => schedules.value?.rounds?.length === 0);
-    const isLoadingSchedules = ref(false);
-    const schedulePagination = ref<IPagination & { filterBy?: RoundStatus | string, search?: string }>({
-        currentPage: 1,
-        perPage: 10,
-        lastPage: 1,
-        total: 0,
-        sort: "asc",
-        filterBy: undefined,
-        search: undefined,
-    })
-    const scheduleSettings = ref<ScheduleSettings>({
-        start_date: new Date(),
-        end_date: null,
-        round_trip: false,
-        elimination_round_trip: true,
-        game_time: 0,
-        min_teams: 0,
-        max_teams: 0,
-        time_between_games: 0,
-        teams: 0,
-        format: {} as Format,
-        footballType: {} as FootballType,
-        locations: [],
-        tiebreakers: [],
-        phases: [] as EliminationPhase[]
-    });
-    const scheduleStoreRequest = ref<ScheduleStoreRequest>({
-        general: {} as FormGeneralScheduleRequest,
-        regular_phase: {} as FormRegularPhaseStep,
-        elimination_phase: {} as FormEliminationPhaseStep,
-        fields_phase: [] as FormLocationAvailabilityStep[],
-    });
-    const calendarSteps = ref<CalendarStepsForm>({
-        current: "general",
-        steps: [
-            {
-                step: "general",
-                completed: false,
-                label: "General",
-            },
-            {
-                step: "regular",
-                completed: false,
-                label: "Fase Regular",
-            },
-            {
-                step: "elimination",
-                completed: false,
-                label: "Fase de Eliminación",
-            },
-            {
-                step: "fields",
-                completed: false,
-                label: "Campos de juego",
-            }
-        ],
-    });
-
-    const scheduleRoundStatus = ref<ScheduleRoundStatus[]>([
-        {value: 'programado', text: 'Programada'},
-        {value: 'en_progreso', text: 'En progreso'},
-        {value: 'completado', text: 'Completada'},
-        {value: 'cancelado', text: 'Cancelada'}
-    ])
-    const getTournamentSchedules = async () => {
-        isLoadingSchedules.value = true
-        const client = useSanctumClient();
-        let url = `/api/v1/admin/tournaments/${tournamentStore.tournamentId}/schedule?page=${schedulePagination.value.currentPage}`;
-
-        if (schedulePagination.value.filterBy) {
-            url += `&filterBy=${schedulePagination.value.filterBy}`;
-        }
-        if (schedulePagination.value.search) {
-            url += `&search=${schedulePagination.value.search}`;
-        }
-        
-
-        const response = await client(url);
-        const newRounds = response.rounds ?? [];
-        if (!schedules.value.rounds) {
-            schedules.value.rounds = [];
-        }
-        schedules.value.tournament = response.tournament;
-        schedules.value.rounds.push(...newRounds);
-        schedulePagination.value.currentPage += 1;
-        schedulePagination.value.lastPage = response.pagination.total_rounds;
-    }
-    const fetchSchedule = async () => {
-        isLoadingSchedules.value = true;
-        const client = useSanctumClient();
-        schedules.value = await client(`/api/v1/admin/tournaments/${tournamentStore.tournamentId}/schedule?page=${schedulePagination.value.currentPage}`)
-            .finally(() => {
-                    isLoadingSchedules.value = false;
-                }
-            );
+  };
+  const generateSchedule = async () => {
+    const client = useSanctumClient();
+    await client(
+      `/api/v1/admin/tournaments/${tournamentStore.tournamentId}/schedule`,
+      {
+        method: 'POST',
+        body: JSON.stringify(scheduleStoreRequest.value),
+      }
+    );
+  };
+  const settingsSchedule = async () => {
+    const client = useSanctumClient();
+    const { data } = (await useAsyncData<ScheduleSettings>(
+      'tournament-settings',
+      () =>
+        client(
+          `api/v1/admin/tournaments/${tournamentStore.tournamentId}/schedule/settings`
+        )
+    )) as { data: Ref<ScheduleSettings> };
+    const generalSchedule = {} as FormGeneralScheduleRequest;
+    generalSchedule.tournament_id = tournamentStore.tournamentId as number;
+    generalSchedule.tournament_format_id = data.value.format.id;
+    generalSchedule.football_type_id = data.value.footballType.id;
+    generalSchedule.start_date = data.value.start_date;
+    generalSchedule.game_time = data.value.game_time;
+    generalSchedule.time_between_games = data.value.time_between_games;
+    generalSchedule.total_teams = data.value.teams;
+    generalSchedule.locations = [];
+    scheduleStoreRequest.value.general = generalSchedule;
+    scheduleStoreRequest.value.regular_phase = {
+      round_trip: data.value.round_trip,
+      tiebreakers: data.value.tiebreakers,
     };
-    const updateStatusGame = async (roundId: number, status: RoundStatus, tournamentId: number) => {
-        const client = useSanctumClient()
-        await client(`api/v1/admin/tournaments/${tournamentId}/schedule/rounds/${roundId}`, {
-            method: 'PUT',
-            body: {
-                status
-            }
-        })
-    }
-    const generateSchedule = async () => {
-        const client = useSanctumClient();
-        await client(`/api/v1/admin/tournaments/${tournamentStore.tournamentId}/schedule`, {
-            method: 'POST',
-            body: JSON.stringify(scheduleStoreRequest.value),
-        });
+    scheduleStoreRequest.value.elimination_phase = {
+      teams_to_next_round: 8,
+      round_trip: false,
+      phases: data.value.phases,
     };
-    const settingsSchedule = async () => {
-        const client = useSanctumClient();
-        const {data} = await useAsyncData<ScheduleSettings>('tournament-settings', () =>
-            client(
-                `api/v1/admin/tournaments/${tournamentStore.tournamentId}/schedule/settings`,
-            )
-        ) as { data: Ref<ScheduleSettings> };
-        const generalSchedule = {} as FormGeneralScheduleRequest
-        generalSchedule.tournament_id = tournamentStore.tournamentId as number;
-        generalSchedule.tournament_format_id = data.value.format.id;
-        generalSchedule.football_type_id = data.value.footballType.id;
-        generalSchedule.start_date = data.value.start_date;
-        generalSchedule.game_time = data.value.game_time;
-        generalSchedule.time_between_games = data.value.time_between_games;
-        generalSchedule.total_teams = data.value.teams;
-        generalSchedule.locations = [];
-        scheduleStoreRequest.value.general = generalSchedule
-        scheduleStoreRequest.value.regular_phase = {
-            round_trip: data.value.round_trip,
-            tiebreakers: data.value.tiebreakers,
-        }
-        scheduleStoreRequest.value.elimination_phase = {
-            teams_to_next_round: 8,
-            round_trip: false,
-            phases: data.value.phases
-        }
-        scheduleSettings.value = data.value;
-    }
-    onMounted(async () => {
-        if (useRoute().name === 'torneos-torneo-calendario')
-            await getTournamentSchedules()
-    })
+    scheduleSettings.value = data.value;
+  };
+  onMounted(async () => {
+    if (useRoute().name === 'torneos-torneo-calendario')
+      await getTournamentSchedules();
+  });
 
-    return {
-        scheduleDialog,
-        daysToPlay,
-        scheduleParams,
-        daysToPlayCustomSelected,
-        daysToPlaySelected,
-        schedules,
-        noSchedules,
-        schedulePagination,
-        scheduleSettings,
-        isLoadingSchedules,
-        scheduleRoundStatus,
-        calendarSteps,
-        scheduleStoreRequest,
-        updateStatusGame,
-        getTournamentSchedules,
-        fetchSchedule,
-        generateSchedule,
-        settingsSchedule,
-    };
+  return {
+    scheduleDialog,
+    daysToPlay,
+    scheduleParams,
+    daysToPlayCustomSelected,
+    daysToPlaySelected,
+    schedules,
+    noSchedules,
+    schedulePagination,
+    scheduleSettings,
+    isLoadingSchedules,
+    scheduleRoundStatus,
+    calendarSteps,
+    scheduleStoreRequest,
+    updateStatusGame,
+    getTournamentSchedules,
+    fetchSchedule,
+    generateSchedule,
+    settingsSchedule,
+    $resetScheduleStore,
+  };
 });
