f20c44138955c53781cee8bee36beed9d61c67eb
Jose Terrazas <jozaguts@gmail.com>
2025-05-22 19:39:17 -0600

    feat(calendario): fetch tournament schedules on mount and reset store on unmount

--- pages/torneos/[torneo]/calendario.vue
+++ pages/torneos/[torneo]/calendario.vue
@@ -1,29 +1,34 @@
 <script lang="ts" setup>
-import AppBar from "~/components/layout/AppBar.vue";
-import PageLayout from "~/components/shared/PageLayout.vue";
-import AppBarBtn from "~/components/pages/torneos/torneo/schedule/AppBarBtn.vue";
-import NoCalendar from "~/components/pages/torneos/no-calendar.vue";
-import Schedule from "~/components/pages/torneos/torneo/schedule/index.vue";
+  import AppBar from '~/components/layout/AppBar.vue'
+  import PageLayout from '~/components/shared/PageLayout.vue'
+  import AppBarBtn from '~/components/pages/torneos/torneo/schedule/AppBarBtn.vue'
+  import NoCalendar from '~/components/pages/torneos/no-calendar.vue'
+  import Schedule from '~/components/pages/torneos/torneo/schedule/index.vue'
+  import { useScheduleStore } from '~/store'
 
-definePageMeta({
-  middleware: [
-    'check-tournament'
-  ],
-});
+  definePageMeta({
+    middleware: ['check-tournament'],
+  })
+  onMounted(async () => {
+    await useScheduleStore().getTournamentSchedules()
+  })
+  onBeforeUnmount(() => {
+    useScheduleStore().$resetScheduleStore()
+  })
 </script>
 <template>
   <PageLayout>
     <template #app-bar>
       <AppBar>
         <template #buttons>
-          <AppBarBtn/>
+          <AppBarBtn />
         </template>
       </AppBar>
     </template>
     <template #default>
-      <NoCalendar/>
-      <LazyPagesTorneosCalendarioDialog/>
-      <Schedule/>
+      <NoCalendar />
+      <LazyPagesTorneosCalendarioDialog />
+      <Schedule />
     </template>
   </PageLayout>
 </template>
