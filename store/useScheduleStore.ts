2ee89c4e88596ccfcc490f42e6dd933ccca883d2
Jose Terrazas <jozaguts@gmail.com>
2025-06-04 12:40:20 -0600

    feat(schedule): add getMatch function to fetch match details

--- store/useScheduleStore.ts
+++ store/useScheduleStore.ts
@@ -120,6 +120,10 @@ export const useScheduleStore = defineStore('scheduleStore', () => {
         {value: 'cancelado', text: 'Cancelada'},
     ]);
 
+    const getMatch = async (matchId: number, fieldId: number, date: string,) => {
+        const client = useSanctumClient();
+        return await client(`/api/v1/admin/games/${matchId}?date=${date}&field_id=${fieldId}`)
+    }
     const $resetScheduleStore = () => {
         scheduleDialog.value = false;
         scheduleParams.value = undefined;
@@ -300,5 +304,6 @@ export const useScheduleStore = defineStore('scheduleStore', () => {
         generateSchedule,
         settingsSchedule,
         $resetScheduleStore,
+        getMatch,
     };
 });
