917ac37d3bef1ebac5c868a745580e8b52824a94
Jose Terrazas <jozaguts@gmail.com>
2025-06-16 07:36:08 -0600

    refactor(basic-info): clean up formatting and improve readability

--- components/pages/jugadores/stepper/01-basic-info.vue
+++ components/pages/jugadores/stepper/01-basic-info.vue
@@ -2,16 +2,16 @@
 import DragDropImage from "~/components/pages/torneos/drag-drop-image.vue";
 import useSchemas from "~/composables/useSchemas";
 import "@vuepic/vue-datepicker/dist/main.css";
-import { usePlayerStore, useTeamStore } from "~/store";
-import { useCategoryStore } from "~/store/useCategoryStore";
-import { dragDropImageRef } from "~/composables/useImage";
+import {usePlayerStore, useTeamStore} from "~/store";
+import {useCategoryStore} from "~/store/useCategoryStore";
+import {dragDropImageRef} from "~/composables/useImage";
 
-const { isEdition, playerStoreRequest } = storeToRefs(usePlayerStore());
-const { teams } = storeToRefs(useTeamStore());
-const { categories } = storeToRefs(useCategoryStore());
-const { handleSubmit, resetForm, fields, validate, setValues } = useSchemas(
-  isEdition.value ? "edit-player-basic-info" : "create-player-basic-info",
-  { nationality: "Mexicana" },
+const {isEdition, playerStoreRequest} = storeToRefs(usePlayerStore());
+const {teams} = storeToRefs(useTeamStore());
+const {categories} = storeToRefs(useCategoryStore());
+const {handleSubmit, resetForm, fields, validate, setValues} = useSchemas(
+    isEdition.value ? "edit-player-basic-info" : "create-player-basic-info",
+    {nationality: "Mexicana"},
 );
 const updateCategory = (teamId: number) => {
   const team = teams.value?.find((team) => team.id === teamId);
@@ -22,7 +22,7 @@ const updateCategory = (teamId: number) => {
 onMounted(() => {
   useTeamStore().list();
   if (playerStoreRequest.value?.basic) {
-    setValues({ ...playerStoreRequest.value.basic });
+    setValues({...playerStoreRequest.value.basic});
     if (playerStoreRequest.value.basic.image) {
       dragDropImageRef.value?.loadImage();
     }
@@ -40,63 +40,64 @@ defineExpose({
 <template>
   <v-container class="pt-0">
     <BaseInput
-      label="Nombre(s) del jugador*"
-      placeholder="p.ej. Cristiano"
-      v-model="fields.name"
+        label="Nombre(s) del jugador*"
+        placeholder="p.ej. Cristiano"
+        v-model="fields.name"
     ></BaseInput>
     <BaseInput
-      placeholder="p.ej. Ronaldo"
-      label="Apellido(s) del jugador*"
-      v-model="fields.last_name"
+        placeholder="p.ej. Ronaldo"
+        label="Apellido(s) del jugador*"
+        v-model="fields.last_name"
     />
     <BaseInput label="Fecha de nacimiento*">
       <template #input>
         <BaseCalendarInput
-          v-model:start_date="fields.birthdate.fieldValue"
-          :multiCalendar="false"
+            v-model:start_date="fields.birthdate.fieldValue"
+            :multiCalendar="false"
         />
+        <span class="text-error ml-4 text-caption">{{ fields.birthdate.fieldPropsValue['error-messages'][0] }}</span>
       </template>
     </BaseInput>
     <BaseInput
-      v-model="fields.nationality"
-      label="Nacionalidad*"
-      placeholder="p.ej. Mexicana"
+        v-model="fields.nationality"
+        label="Nacionalidad*"
+        placeholder="p.ej. Mexicana"
     ></BaseInput>
     <BaseInput label="Imagen del jugador">
       <template #input>
-        <DragDropImage v-model="fields.image.fieldValue" />
+        <DragDropImage v-model="fields.image.fieldValue"/>
         <span
-          class="text-error text-caption"
-          :class="
+            class="text-error text-caption"
+            :class="
             fields.image.fieldPropsValue['error-messages'][0] ? 'ml-2' : ''
           "
-          >{{ fields.image.fieldPropsValue["error-messages"][0] ?? "" }}</span
+        >{{ fields.image.fieldPropsValue["error-messages"][0] ?? "" }}</span
         >
       </template>
     </BaseInput>
     <BaseInput label="Equipo">
       <template #input>
         <v-autocomplete
-          item-value="id"
-          item-title="name"
-          v-model="fields.team_id.fieldValue"
-          density="compact"
-          v-bind="fields.team_id.fieldPropsValue"
-          :items="teams"
-          @update:model-value="updateCategory"
+            item-value="id"
+            item-title="name"
+            v-model="fields.team_id.fieldValue"
+            density="compact"
+            v-bind="fields.team_id.fieldPropsValue"
+            :items="teams"
+            @update:model-value="updateCategory"
         />
       </template>
     </BaseInput>
     <BaseInput label="Categoría">
       <template #input>
         <v-select
-          item-value="id"
-          item-title="name"
-          v-model="fields.category_id.fieldValue"
-          density="compact"
-          disabled
-          v-bind="fields.category_id.fieldPropsValue"
-          :items="categories"
+            item-value="id"
+            item-title="name"
+            v-model="fields.category_id.fieldValue"
+            density="compact"
+            disabled
+            v-bind="fields.category_id.fieldPropsValue"
+            :items="categories"
         />
       </template>
     </BaseInput>
