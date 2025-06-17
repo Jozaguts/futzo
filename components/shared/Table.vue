f89ddb9af67fb490179686780d9530f0dbba46a8
Jose Terrazas <jozaguts@gmail.com>
2025-06-17 13:10:42 -0600

    feat(table): add team assignment functionality and improve prop definitions

--- components/shared/Table.vue
+++ components/shared/Table.vue
@@ -1,81 +1,90 @@
 <script lang="ts" setup>
-  import type { IPagination } from '~/interfaces'
-  import CopyLink from '~/components/shared/copy-link.vue'
+import type {IPagination} from '~/interfaces'
+import CopyLink from '~/components/shared/copy-link.vue'
 
-  type Header = {
-    title: string
-    value: string
-    align?: 'start' | 'center' | 'end'
-    sortable?: boolean
-    filterable?: boolean
-    divider?: boolean
-  }
+type Header = {
+  title: string
+  value: string
+  align?: 'start' | 'center' | 'end'
+  sortable?: boolean
+  filterable?: boolean
+  divider?: boolean
+}
 
-  defineProps({
-    headers: {
-      type: Array as PropType<Header[]>,
-      required: true,
-    },
-    items: {
-      type: Array,
-      required: true,
-    },
-    itemKey: {
-      type: String,
-      default: 'name',
-    },
-    statusHandler: {
-      type: Function as PropType<(item: string) => string>,
-      required: false,
-    },
-    showIndex: {
-      type: Boolean,
-      default: false,
-    },
-    customName: {
-      type: Boolean,
-      default: false,
-    },
-    showFooter: {
-      type: Boolean,
-      default: true,
-    },
-    paginate: {
-      type: Function,
-      required: true,
-    },
-    showLink: {
-      type: Boolean,
-      default: false,
-    },
-  })
-  const search = defineModel('search', {
+defineProps({
+  headers: {
+    type: Array as PropType<Header[]>,
+    required: true,
+  },
+  items: {
+    type: Array,
+    required: true,
+  },
+  itemKey: {
     type: String,
+    default: 'name',
+  },
+  statusHandler: {
+    type: Function as PropType<(item: string) => string>,
     required: false,
-  })
-  const pagination = defineModel<IPagination>('pagination', { required: true })
+  },
+  showIndex: {
+    type: Boolean,
+    default: false,
+  },
+  customName: {
+    type: Boolean,
+    default: false,
+  },
+  showFooter: {
+    type: Boolean,
+    default: true,
+  },
+  paginate: {
+    type: Function,
+    required: true,
+  },
+  showLink: {
+    type: Boolean,
+    default: false,
+  },
+  assignTeam: {
+    type: Function as PropType<(item: any) => void>,
+    required: false,
+  },
+  enableAssignTeam: {
+    type: Boolean,
+    default: false,
+    required: false,
+  },
+})
+const search = defineModel('search', {
+  type: String,
+  required: false,
+})
+const pagination = defineModel<IPagination>('pagination', {required: true})
 </script>
 <template>
   <v-data-table
-    class="border-sm futzo-rounded"
-    :headers="headers"
-    :items="items"
-    :search="search"
-    :item-key="itemKey"
-    items-per-page="10"
-    show-select
-    :cell-props="{ class: 'text-capitalize' }"
-    height="100%"
+      class="border-sm futzo-rounded"
+      :headers="headers"
+      :items="items"
+      :search="search"
+      :item-key="itemKey"
+      items-per-page="10"
+      show-select
+      :cell-props="{ class: 'text-capitalize' }"
+      height="100%"
   >
     <!--    header-->
     <template
-      v-slot:header.data-table-select="{ allSelected, selectAll, someSelected }"
+        v-slot:header.data-table-select="{ allSelected, selectAll, someSelected }"
     >
       <v-checkbox-btn
-        :indeterminate="someSelected && !allSelected"
-        :model-value="allSelected"
-        color="primary"
-        @update:model-value="selectAll(!allSelected)"
+          :indeterminate="someSelected && !allSelected"
+          :model-value="allSelected"
+          color="primary"
+          @update:model-value="selectAll(!allSelected)"
       >
       </v-checkbox-btn>
     </template>
@@ -100,13 +109,22 @@
         </template>
       </v-tooltip>
     </template>
+    <template v-slot:item.team.name="{item}">
+      <v-btn :disabled="!enableAssignTeam"
+             density="compact"
+             v-if="!item?.team?.name"
+             @click="() => assignTeam(item)"
+      >Asignar equipo
+      </v-btn>
+      <span v-else>{{ item.team.name }}</span>
+    </template>
     <template
-      v-slot:item.data-table-select="{ internalItem, isSelected, toggleSelect }"
+        v-slot:item.data-table-select="{ internalItem, isSelected, toggleSelect }"
     >
       <v-checkbox-btn
-        :model-value="isSelected(internalItem)"
-        color="primary"
-        @update:model-value="toggleSelect(internalItem)"
+          :model-value="isSelected(internalItem)"
+          color="primary"
+          @update:model-value="toggleSelect(internalItem)"
       ></v-checkbox-btn>
     </template>
     <template v-slot:item.status="{ item }">
@@ -121,28 +139,28 @@
     </template>
     <!--    link }}-->
     <template v-if="showLink" #[`item.link`]="{ item }">
-      <CopyLink :item="item" />
+      <CopyLink :item="item"/>
     </template>
     <!--    link -->
     <template #bottom>
-      <v-divider />
+      <v-divider/>
       <v-pagination
-        v-if="showFooter"
-        class="futzo-pagination"
-        v-model="pagination.currentPage"
-        :length="pagination.lastPage"
-        start="1"
-        @update:modelValue="() => paginate()"
+          v-if="showFooter"
+          class="futzo-pagination"
+          v-model="pagination.currentPage"
+          :length="pagination.lastPage"
+          start="1"
+          @update:modelValue="() => paginate()"
       >
         <template #prev="props">
           <v-btn
-            @click="props.onClick"
-            :disabled="props.disabled"
-            elevation="0"
-            variant="text"
-            color="black"
-            rounded="md"
-            border="thin secondary"
+              @click="props.onClick"
+              :disabled="props.disabled"
+              elevation="0"
+              variant="text"
+              color="black"
+              rounded="md"
+              border="thin secondary"
           >
             <template #prepend>
               <Icon name="futzo-icon:arrow-left"></Icon>
@@ -152,14 +170,14 @@
         </template>
         <template #next="props">
           <v-btn
-            @click="props.onClick"
-            :disabled="props.disabled"
-            elevation="0"
-            variant="text"
-            color="black"
-            rounded="md"
-            border="thin secondary"
-            class="ml-auto"
+              @click="props.onClick"
+              :disabled="props.disabled"
+              elevation="0"
+              variant="text"
+              color="black"
+              rounded="md"
+              border="thin secondary"
+              class="ml-auto"
           >
             <template #append>
               <Icon name="futzo-icon:arrow-right"></Icon>
