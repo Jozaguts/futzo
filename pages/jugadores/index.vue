d0efbcb9445476b637495a5b028da6fefeb82d68
Jose Terrazas <jozaguts@gmail.com>
2025-06-17 13:10:49 -0600

    feat(players): add AssignTeamDialog component and clean up spacing

--- pages/jugadores/index.vue
+++ pages/jugadores/index.vue
@@ -1,11 +1,12 @@
 <script lang="ts" setup>
 import AppBar from "~/components/layout/AppBar.vue";
 import PlayersNavbarButtons from "~/components/pages/jugadores/players-navbar-buttons.vue";
-import { usePlayerStore } from "~/store";
+import {usePlayerStore} from "~/store";
 import NoPlayers from "~/components/pages/jugadores/no-players.vue";
 import PlayersDialog from "~/components/pages/jugadores/dialog/index.vue";
 import PlayersTable from "~/components/pages/jugadores/players-table.vue";
 import ImportDialog from "@/components/pages/jugadores/import-dialog/index.vue";
+import AssignTeamDialog from "@/components/pages/jugadores/assign-team-dialog.vue";
 
 onMounted(() => {
   usePlayerStore().getPlayers();
@@ -16,15 +17,16 @@ onMounted(() => {
     <template #app-bar>
       <AppBar>
         <template #buttons>
-          <PlayersNavbarButtons />
+          <PlayersNavbarButtons/>
         </template>
       </AppBar>
     </template>
     <template #default>
-      <NoPlayers />
-      <PlayersTable />
-      <PlayersDialog />
-      <ImportDialog />
+      <NoPlayers/>
+      <PlayersTable/>
+      <PlayersDialog/>
+      <ImportDialog/>
+      <AssignTeamDialog/>
     </template>
   </PageLayout>
 </template>
