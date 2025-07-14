d0bfb5cdb7c216893284cbbd7cd051c9fd8deabe
Jose Terrazas <jozaguts@gmail.com>
2025-07-14 14:26:35 -0600

    feat(index, teams-team): replace PositionsTable with LinesupContainer and migrate styles to SASS

--- pages/equipos/[equipo]/index.vue
+++ pages/equipos/[equipo]/index.vue
@@ -7,6 +7,7 @@ import PageLayout from "~/components/shared/PageLayout.vue";
 import AppBar from "~/components/layout/AppBar.vue";
 import AppBarBtn from "~/components/pages/equipos/equipo/app-bar-btn.vue";
 import CreateTeamDialog from "~/components/pages/equipos/CreateTeamDialog/index.vue";
+import LinesupContainer from "~/components/pages/calendario/game-report/linesup-container.vue";
 </script>
 <template>
   <PageLayout>
@@ -19,8 +20,8 @@ import CreateTeamDialog from "~/components/pages/equipos/CreateTeamDialog/index.
     </template>
     <template #default>
       <div class="tournament-details-container">
-        <div class="table-container">
-          <PositionsTable/>
+        <div class="table-container ">
+          <LinesupContainer/>
         </div>
         <div class="games-container">
           <div class="live-games">
@@ -38,56 +39,6 @@ import CreateTeamDialog from "~/components/pages/equipos/CreateTeamDialog/index.
     </template>
   </PageLayout>
 </template>
-<style lang="scss">
-.tournament-details-container {
-  display: grid;
-  grid-auto-columns: 1fr;
-  grid-template-columns: calc(80% - 20px) calc(20% + 20px);
-  grid-template-rows: calc(70% - 20px) calc(30% + 20px);
-  gap: 1em 1em;
-  grid-template-areas:
-    "table-container games-container"
-    "next-games games-container";
-}
-
-.table-container {
-  grid-area: table-container;
-  padding: 1rem;
-  border-radius: var(--radius-2xl, 16px);
-  background: var(--Colors-Base-White, #fff);
-}
-
-.games-container {
-  grid-area: games-container;
-}
-
-.games-container > :first-child {
-  margin-bottom: 2rem;
-}
-
-.live-games {
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: var(--spacing-xl, 16px);
-}
-
-.next-games-today {
-  display: flex;
-  width: 100%;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: var(--spacing-xl, 16px);
-}
-
-.next-games {
-  grid-area: next-games;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: var(--spacing-md, 8px);
-  align-self: stretch;
-  width: 100%;
-}
+<style lang="sass">
+@use "~@/assets/scss/pages/teams-team.sass"
 </style>
