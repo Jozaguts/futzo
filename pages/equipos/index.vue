8b330fae24fe45a720c7b4c9f81c462702867bf8
Jose Terrazas <jozaguts@gmail.com>
2025-07-14 20:46:29 -0600

    fix(User): correct parameter type hint in registerMediaConversions method

--- pages/equipos/index.vue
+++ pages/equipos/index.vue
@@ -1,31 +1,31 @@
 <script lang="ts" setup>
-import {useTeamStore} from "~/store/useTeamStore";
-import AppBar from "~/components/layout/AppBar.vue";
-import AppBarButtons from "~/components/pages/equipos/team-navbar-buttons.vue";
-import NoTeams from "~/components/pages/equipos/NoTeams.vue";
-import CreateTeamDialog from "~/components/pages/equipos/CreateTeamDialog/index.vue";
-import TeamsTable from "~/components/pages/equipos/teams-table.vue";
-import ImportDialog from "~/components/pages/equipos/import-dialog/index.vue";
+  import { useTeamStore } from '~/store/useTeamStore'
+  import AppBar from '~/components/layout/AppBar.vue'
+  import AppBarButtons from '~/components/pages/equipos/team-navbar-buttons.vue'
+  import NoTeams from '~/components/pages/equipos/NoTeams.vue'
+  import CreateTeamDialog from '~/components/pages/equipos/CreateTeamDialog/index.vue'
+  import TeamsTable from '~/components/pages/equipos/teams-table.vue'
+  import ImportDialog from '~/components/pages/equipos/import-dialog/index.vue'
 
-const teamStore = useTeamStore();
-onMounted(() => {
-  teamStore.getTeams();
-});
+  const teamStore = useTeamStore()
+  onMounted(() => {
+    teamStore.getTeams()
+  })
 </script>
 <template>
   <PageLayout>
     <template #app-bar>
       <AppBar>
         <template #buttons>
-          <AppBarButtons/>
+          <AppBarButtons />
         </template>
       </AppBar>
     </template>
     <template #default>
-      <NoTeams/>
-      <TeamsTable/>
-      <CreateTeamDialog/>
-      <ImportDialog/>
+      <NoTeams />
+      <TeamsTable />
+      <CreateTeamDialog />
+      <ImportDialog />
     </template>
   </PageLayout>
 </template>
