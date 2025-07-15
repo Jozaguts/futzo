20fa9083dc2c4992604f57ef2a33061f90293ea1
Jose Terrazas <jozaguts@gmail.com>
2025-07-15 14:17:26 -0600

    feat(player-dot): enhance player display with dynamic attributes and action icons

--- components/pages/calendario/game-report/player-dot.vue
+++ components/pages/calendario/game-report/player-dot.vue
@@ -1,14 +1,62 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import type { FormationPlayer } from '~/models/Game'
+  defineProps<{
+    player: FormationPlayer
+    id: String
+  }>()
+  const emits = defineEmits(['addPlayer'])
+</script>
 <template>
   <div class="dot-container">
-    <span class="goal-keeper-container">
-      <v-avatar text="1" border="md" color="red" size="32" />
+    <span class="dot-player-container">
+      <v-avatar
+        :text="player?.number?.toString()"
+        size="32"
+        border="lg"
+        color="red"
+        v-if="player?.name"
+      />
+      <v-btn icon density="compact" border="md" @click="$emit('addPlayer', id)">
+        +
+      </v-btn>
+      <Icon
+        name="futzo-icon:dot-player-yellow-card"
+        class="dot-player-card"
+        mode="svg"
+        size="24"
+        v-if="
+          player?.cards?.yellow &&
+          !player?.cards?.red &&
+          !player?.cards?.doble_yellow_card
+        "
+      />
+      <Icon
+        name="futzo-icon:dot-player-red-card"
+        class="dot-player-card"
+        mode="svg"
+        size="24"
+        v-if="player?.cards?.red || player?.cards?.doble_yellow_card"
+      />
+      <Icon
+        name="futzo-icon:dot-player-out"
+        class="dot-player-out"
+        mode="svg"
+        size="24"
+        v-if="player?.substituted"
+      />
+      <Icon
+        name="futzo-icon:dot-player-goal"
+        class="dot-player-goal"
+        mode="svg"
+        size="24"
+        v-if="player?.goals && player?.goals > 0"
+      />
     </span>
     <div>
       <span class="player-name">
-        <span class="d-inline-block text-truncate" style="max-width: 80px"
-          >B. Allan</span
-        >
+        <span class="d-inline-block text-truncate" style="max-width: 80px">{{
+          player?.name
+        }}</span>
       </span>
     </div>
   </div>
