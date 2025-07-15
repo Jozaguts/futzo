7be3bf80bb90e3b752a3835b8fede4d658e876fb
Jose Terrazas <jozaguts@gmail.com>
2025-07-15 10:51:08 -0600

    refactor(index): clean up imports and improve template formatting

--- components/pages/ubicaciones/dialog/index.vue
+++ components/pages/ubicaciones/dialog/index.vue
@@ -1,34 +1,32 @@
 <script lang="ts" setup>
-import HeaderCard from "~/components/pages/ubicaciones/dialog/Header.vue";
-import LocationStepper from "~/components/pages/ubicaciones/stepper/index.vue";
-import {storeToRefs} from "pinia";
-import {useLocationStore} from "~/store";
-import type {LocationStoreRequest} from "~/models/Location";
-import {DEFAULT_AVAILABILITY_HOURS, DEFAULT_POSITION} from "~/utils/constants";
+  import HeaderCard from '~/components/pages/ubicaciones/dialog/Header.vue'
+  import LocationStepper from '~/components/pages/ubicaciones/stepper/index.vue'
+  import { storeToRefs } from 'pinia'
+  import { useLocationStore } from '~/store'
 
-const {locationDialog, formSteps} = storeToRefs(useLocationStore());
-const leaveHandler = () => {
-  useLocationStore().resetLocationStoreRequest();
-  formSteps.value.current = 'location';
-};
-
+  const { locationDialog, formSteps } = storeToRefs(useLocationStore())
+  const leaveHandler = () => {
+    useLocationStore().resetLocationStoreRequest()
+    formSteps.value.current = 'location'
+  }
 </script>
 <template>
   <v-dialog
-      v-model="locationDialog"
-      max-width="690"
-      @after-leave="leaveHandler"
-      scrollable
+    v-model="locationDialog"
+    max-width="690"
+    @after-leave="leaveHandler"
+    scrollable
   >
     <v-card
-        class="create-tournament-card futzo-rounded"
-        height="100%"
-        :style="{ overflow: $vuetify.display.mobile ? '' : 'hidden' }"
+      class="create-tournament-card futzo-rounded"
+      height="100%"
+      :style="{ overflow: $vuetify.display.mobile ? '' : 'hidden' }"
     >
-      <HeaderCard/>
+      <HeaderCard />
       <LocationStepper
-          @close="() => locationDialog = false"
-          @location-added="() => locationDialog = false"/>
+        @close="() => (locationDialog = false)"
+        @location-added="() => (locationDialog = false)"
+      />
     </v-card>
   </v-dialog>
 </template>
