6a174393e33c864fc6db69a1a00a27010f0596eb
Jose Terrazas <jozaguts@gmail.com>
2025-07-16 12:25:17 -0600

    feat: enhance player-dot component with dropdown menu for player selection and refactor linesup-container to utilize watchEffect for home prop

--- components/pages/equipos/CreateTeamDialog/index.vue
+++ components/pages/equipos/CreateTeamDialog/index.vue
@@ -1,37 +1,39 @@
 <script setup lang="ts">
-import HeaderCard from "~/components/pages/equipos/CreateTeamDialog/Header.vue";
-import {storeToRefs} from "pinia";
-import {useTeamStore, useTournamentStore} from "~/store";
-import StepperContainer from "~/components/pages/equipos/stepper/index.vue";
+  import HeaderCard from '~/components/pages/equipos/CreateTeamDialog/Header.vue'
+  import { storeToRefs } from 'pinia'
+  import { useTeamStore, useTournamentStore } from '~/store'
+  import StepperContainer from '~/components/pages/equipos/stepper/index.vue'
 
-const teamStore = useTeamStore();
-const {steps, isEdition, teamStoreRequest} = storeToRefs(teamStore);
-const leaveHandler = () => {
-  steps.value.current = "createTeam";
-  steps.value.steps?.map((step) => step.completed = false);
-  isEdition.value = false;
-  teamStoreRequest.value = {};
-};
-onMounted(() => {
-  useTournamentStore().fetchTournamentsByLeagueId();
-});
+  const teamStore = useTeamStore()
+  const { steps, isEdition, teamStoreRequest } = storeToRefs(teamStore)
+  const leaveHandler = () => {
+    steps.value.current = 'createTeam'
+    steps.value.steps?.map((step) => (step.completed = false))
+    isEdition.value = false
+    teamStoreRequest.value = {}
+  }
+  onMounted(() => {
+    if (teamStore.dialog) {
+      useTournamentStore().fetchTournamentsByLeagueId()
+    }
+  })
 </script>
 <template>
   <v-dialog
-      v-model="teamStore.dialog"
-      max-width="690"
-      @after-leave="leaveHandler"
-      scrollable
+    v-model="teamStore.dialog"
+    max-width="690"
+    @after-leave="leaveHandler"
+    scrollable
   >
     <v-card
-        class="create-tournament-card futzo-rounded"
-        :style="{ overflow: $vuetify.display.mobile ? '' : 'hidden' }"
+      class="create-tournament-card futzo-rounded"
+      :style="{ overflow: $vuetify.display.mobile ? '' : 'hidden' }"
     >
-      <HeaderCard/>
-      <StepperContainer :step="steps.current"/>
+      <HeaderCard />
+      <StepperContainer :step="steps.current" />
     </v-card>
   </v-dialog>
 </template>
 <style lang="sass">
-@use "assets/scss/pages/create-team.sass"
+  @use "assets/scss/pages/create-team.sass"
 </style>
