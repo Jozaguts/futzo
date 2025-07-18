d0eef18de357d67c9c422c68911e48ae736bb737
Jose Terrazas <jozaguts@gmail.com>
2025-07-18 09:56:28 -0600

    style(default.vue, teams-team.sass): clean up formatting and indentation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,24 +1,16 @@
 <template>
-  <NuxtLoadingIndicator color="#9155FD" :height="6"/>
+  <NuxtLoadingIndicator color="#9155FD" :height="6" />
   <div>
     <v-layout>
       <v-app app>
         <ClientOnly>
-          <Toaster position="top-right" offset="80px" :duration="3000"/>
+          <Toaster position="top-right" offset="80px" :duration="3000" />
         </ClientOnly>
         <ClientOnly>
-          <Navigation/>
+          <Navigation />
           <v-main v-show="show" class="v-main" app>
             <slot></slot>
           </v-main>
-          <v-footer
-              color="white"
-              app
-              class="d-flex justify-start align-center"
-              height="64px"
-          >
-            <span class="caption ml-4">© 2021 Futzo</span>
-          </v-footer>
         </ClientOnly>
       </v-app>
     </v-layout>
@@ -26,36 +18,36 @@
 </template>
 
 <script setup lang="ts">
-import Navigation from "~/components/layout/navigation-drawer.vue";
-import {useGlobalStore} from "~/store";
-import {storeToRefs} from "pinia";
-import {Toaster} from "vue-sonner";
+  import Navigation from '~/components/layout/navigation-drawer.vue'
+  import { useGlobalStore } from '~/store'
+  import { storeToRefs } from 'pinia'
+  import { Toaster } from 'vue-sonner'
 
-const show = ref(false);
-const {rail, isMobile} = storeToRefs(useGlobalStore());
-onMounted(() => {
-  show.value = true;
-});
-const paddingLeft = computed(() => {
-  if (isMobile.value) {
-    return "0px";
-  }
-  return rail.value ? "56px" : "256px";
-});
-const isNotConfigurationPage = computed(() => {
-  return useRoute().name !== "configuracion";
-});
+  const show = ref(false)
+  const { rail, isMobile } = storeToRefs(useGlobalStore())
+  onMounted(() => {
+    show.value = true
+  })
+  const paddingLeft = computed(() => {
+    if (isMobile.value) {
+      return '0px'
+    }
+    return rail.value ? '56px' : '256px'
+  })
+  const isNotConfigurationPage = computed(() => {
+    return useRoute().name !== 'configuracion'
+  })
 </script>
 <style>
-.v-main {
-  padding-left: v-bind(paddingLeft);
-  padding-bottom: 64px;
-  position: relative;
-}
-
-@media (min-width: 920px) {
   .v-main {
     padding-left: v-bind(paddingLeft);
+    padding-bottom: 64px;
+    position: relative;
   }
-}
+
+  @media (min-width: 920px) {
+    .v-main {
+      padding-left: v-bind(paddingLeft);
+    }
+  }
 </style>
