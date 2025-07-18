e121578968a636bcfa32633e4ca0082deb20f9a9
Jose Terrazas <jozaguts@gmail.com>
2025-07-18 07:34:33 -0600

    feat(players-menu.vue): enhance player addition functionality and update team formation handling

--- components/pages/calendario/game-report/players-menu.vue
+++ components/pages/calendario/game-report/players-menu.vue
@@ -1,13 +1,32 @@
 <script lang="ts" setup>
-  import { usePlayerStore } from '~/store'
+  import { usePlayerStore, useTeamStore } from '~/store'
+  import type { TeamLineupAvailablePlayers } from '~/models/Player'
+  import type { Formation, FormationPlayer } from '~/models/Game'
+  import { getTeamFormation } from '~/http/api/team'
+  import type { Team } from '~/models/Team'
   const { defaultLineupAvailableTeamPlayers } = storeToRefs(usePlayerStore())
-  defineProps<{
+  const { homeFormation, awayFormation } = storeToRefs(useTeamStore())
+  const { icon, field_location, number, currentPlayer } = defineProps<{
     icon: string
     number: number
+    field_location: number
+    currentPlayer: FormationPlayer
   }>()
+  const addPlayer = async (player: TeamLineupAvailablePlayers) => {
+    await usePlayerStore().updateDefaultLineup(
+      player,
+      currentPlayer,
+      field_location
+    )
+    getTeamFormation({ id: player.team_id } as Team).then(
+      (response: Formation) => {
+        homeFormation.value = response
+      }
+    )
+  }
 </script>
 <template>
-  <v-menu max-height="150" location="start">
+  <v-menu max-height="250" location="start">
     <template v-slot:activator="{ props }">
       <v-btn staked icon size="32" border="lg" color="red" readonly>
         <v-badge
@@ -20,19 +39,19 @@
           <template #badge>
             <Icon :name="icon"></Icon>
           </template>
-          {{ number }}
-        </v-badge>
+          {{ number }}</v-badge
+        >
       </v-btn>
     </template>
     <v-list density="compact">
       <v-list-item
-        v-for="(item, index) in defaultLineupAvailableTeamPlayers"
+        v-for="(player, index) in defaultLineupAvailableTeamPlayers"
         :key="index"
-        :value="item"
-        @click="$emit('addPlayer', { id, player: item })"
+        :value="player"
+        @click="addPlayer(player)"
       >
         <v-list-item-title
-          >{{ item?.name }} | {{ item?.position }}</v-list-item-title
+          >{{ player?.name }} | {{ player?.position }}</v-list-item-title
         >
       </v-list-item>
     </v-list>
