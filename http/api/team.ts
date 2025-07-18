f27d7f33029bf2f4d6f8a2b1450e234dbbe714e0
Jose Terrazas <jozaguts@gmail.com>
2025-07-18 07:34:41 -0600

    feat(team.ts): add updateDefaultLineup function for modifying default lineup players

--- http/api/team.ts
+++ http/api/team.ts
@@ -1,5 +1,6 @@
 import type { Team } from '~/models/Team';
 import type { TeamLineupAvailablePlayers } from '~/models/Player';
+import type { FormationPlayer } from '~/models/Game';
 
 export const getTeamBy = async (term: number | string) => {
   const client = useSanctumClient();
@@ -7,7 +8,7 @@ export const getTeamBy = async (term: number | string) => {
     `/api/v1/admin/teams/${term}?by_slug=${typeof term === 'string'}`
   );
 };
-export const geDefaultLineupAvailableTeemPlayers = async (team: Team) => {
+export const getDefaultLineupAvailableTeemPlayers = async (team: Team) => {
   const client = useSanctumClient();
   return await client<Promise<TeamLineupAvailablePlayers[]>>(
     `/api/v1/admin/teams/${team.id}/available-players`
@@ -16,4 +17,18 @@ export const geDefaultLineupAvailableTeemPlayers = async (team: Team) => {
 export const getTeamFormation = async (team: Team) => {
   const client = useSanctumClient();
   return await client(`/api/v1/admin/teams/${team.id}/formation`);
+};
+export const updateDefaultLineup = async (
+  player: TeamLineupAvailablePlayers,
+  currentPlayer: FormationPlayer,
+  field_location: number
+) => {
+  const client = useSanctumClient();
+  return await client<Promise<TeamLineupAvailablePlayers[]>>(
+    `/api/v1/admin/teams/${player.team_id}/default-lineup-players/${currentPlayer.default_lineup_player_id}`,
+    {
+      method: 'PUT',
+      body: JSON.stringify({ player, field_location }),
+    }
+  );
 };
