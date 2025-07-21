782393af63a0f263a5c9f8c372fd851a21cb73ac
Jose Terrazas <jozaguts@gmail.com>
2025-07-21 12:19:15 -0600

    refactor(linesup-container.vue): streamline team formation handling and improve event emissions

--- components/pages/calendario/game-report/linesup-container.vue
+++ components/pages/calendario/game-report/linesup-container.vue
@@ -1,68 +1,53 @@
 <script lang="ts" setup>
   import PlayerDot from '~/components/pages/calendario/game-report/player-dot.vue'
   import type { TeamFormation } from '~/models/Game'
-  import { getTeamFormation } from '~/http/api/team'
-  import { usePlayerStore, useTeamStore } from '~/store'
   import type { Formation, Team } from '~/models/Team'
-  import { sortFormation } from '~/utils/sort-formation'
-  const { showComplete, isReport } = defineProps({
+
+  const {
+    showComplete,
+    isReport,
+    homeTeam,
+    awayTeam,
+    homeFormation,
+    awayFormation,
+    formations,
+  } = defineProps({
     showComplete: Boolean,
     isReport: Boolean,
+    homeTeam: {
+      type: Object as PropType<Team>,
+    },
+    awayTeam: {
+      type: Object as PropType<Team>,
+    },
+    homeFormation: {
+      type: Object as PropType<TeamFormation>,
+    },
+    awayFormation: {
+      type: Object as PropType<TeamFormation>,
+    },
+    formations: {
+      type: Array as PropType<Formation[]>,
+    },
   })
-  const { homeTeam, awayTeam, homeFormation, awayFormation, formations } =
-    storeToRefs(useTeamStore())
-
-  watch([homeTeam, awayTeam], async ([newHomeTeam, newAwayTeam]) => {
-    if (isReport) return
-    if (!!newHomeTeam?.id) {
-      getTeamFormation(newHomeTeam as Team).then((response: TeamFormation) => {
-        response = sortFormation(response)
-        homeFormation.value = response
-      })
-      await usePlayerStore().getDefaultLineupAvailableTeamPlayers(newHomeTeam)
-    }
-    if (!!newAwayTeam?.id) {
-      getTeamFormation(newAwayTeam as Team).then((response: TeamFormation) => {
-        awayFormation.value = response
-      })
-      await usePlayerStore().getDefaultLineupAvailableTeamPlayers(newAwayTeam)
-    }
-  })
+  const emits = defineEmits(['updateFormationType', 'leaving'])
   const formationDetails = computed(() => {
     return {
-      home: formations.value.find(
-        (formation) => formation.name === homeFormation.value?.name
+      home: formations?.find(
+        (formation) => formation.name === homeFormation?.name
       ),
-      away: formations.value.find(
-        (formation) => formation.name === awayFormation.value?.name
+      away: formations?.find(
+        (formation) => formation.name === awayFormation?.name
       ),
     }
   })
-  const updateFormationType = async (team_id: number, formation_id: number) => {
-    await useTeamStore()
-      .updateFormationType(team_id, formation_id)
-      .then(() => {
-        getTeamFormation({ id: team_id } as Team).then(
-          (response: TeamFormation) => {
-            response = sortFormation(response)
-            homeFormation.value = response
-          }
-        )
-      })
-  }
   const linesupHeightContainer = computed(() => {
     return showComplete ? '880px' : '100%'
   })
   const linesupTeamHeightContainerStyle = computed(() => {
     return showComplete ? '50%' : '100%'
   })
-  onUnmounted(() => {
-    homeTeam.value = {} as Team
-    awayTeam.value = {} as Team
-    homeFormation.value = {} as TeamFormation
-    awayFormation.value = {} as TeamFormation
-    formations.value = [] as Formation[]
-  })
+  onUnmounted(() => emits('leaving'))
 </script>
 <template>
   <v-sheet class="linesup-container">
@@ -82,7 +67,12 @@
             item-value="id"
             @update:model-value="
               (value) =>
-                updateFormationType(Number(homeTeam?.id), Number(value))
+                emits(
+                  'updateFormationType',
+                  true,
+                  Number(homeTeam?.id),
+                  Number(value)
+                )
             "
           >
           </v-select>
@@ -167,6 +157,71 @@
         <div class="zone-2-away"></div>
         <div class="zone-3-away"></div>
         <div class="zone-4-away"></div>
+        <div class="midfield">
+          <div class="row-lineup">
+            <div
+              class="d-flex justify-center justify-space-around"
+              v-auto-animate
+            >
+              <PlayerDot
+                v-for="(player, index) in awayFormation?.forwards"
+                :key="index"
+                :player="player"
+                :field_location="
+                  Number(index + 1) +
+                  Number(formationDetails?.away?.defenses ?? 0) +
+                  Number(formationDetails?.away?.midfielders ?? 0) +
+                  Number(formationDetails?.away?.goalkeeper ?? 0)
+                "
+              />
+            </div>
+          </div>
+          <div class="row-lineup">
+            <div
+              class="d-flex justify-center justify-space-around"
+              v-auto-animate
+            >
+              <PlayerDot
+                v-for="(player, index) in awayFormation?.midfielders"
+                :key="index"
+                :player="player"
+                :field_location="
+                  Number(index + 1) +
+                  Number(formationDetails?.away?.defenses ?? 0) +
+                  Number(formationDetails?.away?.goalkeeper ?? 0)
+                "
+              />
+            </div>
+          </div>
+          <div class="row-lineup">
+            <div class="players-row-container">
+              <div
+                class="d-flex justify-center justify-space-around"
+                v-auto-animate
+              >
+                <PlayerDot
+                  v-for="(player, index) in awayFormation?.defenses"
+                  :key="index"
+                  :player="player"
+                  :field_location="
+                    Number(index + 1) +
+                    Number(formationDetails?.away?.goalkeeper ?? 0)
+                  "
+                />
+              </div>
+            </div>
+          </div>
+          <div class="row-lineup">
+            <div class="players-row-container">
+              <PlayerDot
+                v-for="(player, index) in awayFormation?.goalkeeper"
+                :key="index"
+                :player="player"
+                :field_location="Number(formationDetails?.away?.goalkeeper)"
+              />
+            </div>
+          </div>
+        </div>
       </div>
       <div class="heading">
         <v-avatar :image="awayTeam?.image" class="mx-4" size="32"></v-avatar>
@@ -182,7 +237,12 @@
             item-value="id"
             @update:model-value="
               (value) =>
-                updateFormationType(Number(awayTeam?.id), Number(value))
+                emits(
+                  'updateFormationType',
+                  false,
+                  Number(awayTeam?.id),
+                  Number(value)
+                )
             "
           >
           </v-select>
