7a5127a5de90abece4c109cc6a1bd116e58cdfcb
Jose Terrazas <jozaguts@gmail.com>
2025-08-03 12:12:49 -0600

    refactor(components): table image size

--- components/pages/equipos/teams-table.vue
+++ components/pages/equipos/teams-table.vue
@@ -1,36 +1,27 @@
 <script setup lang="ts">
-import {useTeamStore} from "~/store";
-import getHeaders from "~/utils/headers-table";
+  import { useTeamStore } from '~/store'
+  import getHeaders from '~/utils/headers-table'
 
-const {
-  teams,
-  pagination,
-  search,
-} = storeToRefs(useTeamStore());
+  const { teams, pagination, search } = storeToRefs(useTeamStore())
 
-const headers = getHeaders("teams");
+  const headers = getHeaders('teams')
 </script>
 <template>
   <Table
-      v-if="teams?.length"
-      :headers="headers"
-      :show-index="true"
-      :items="teams"
-      itemKey="name"
-      :search.sync="search"
-      v-model:pagination="pagination"
-      :paginate="useTeamStore().getTeams"
-      :custom-name="true"
-      :show-link="true"
+    v-if="teams?.length"
+    :headers="headers"
+    :show-index="true"
+    :items="teams"
+    itemKey="name"
+    :search.sync="search"
+    v-model:pagination="pagination"
+    :paginate="useTeamStore().getTeams"
+    :custom-name="true"
+    :show-link="true"
   >
     <template #actions="{ item }">
-      <v-btn
-          size="small"
-          rounded="md"
-          variant="outlined"
-          class="table-action-btn"
-          :to="`/equipos/${item.slug}`"
-      >Ver Equipo
+      <v-btn size="small" rounded="md" variant="outlined" class="table-action-btn" :to="`/equipos/${item?.slug}`"
+        >Ver Equipo
       </v-btn>
     </template>
   </Table>
--- components/shared/Table.vue
+++ components/shared/Table.vue
@@ -1,81 +1,79 @@
 <script lang="ts" setup>
-import type {Header, IPagination} from '~/interfaces'
-import CopyLink from '~/components/shared/copy-link.vue'
+  import type { Header, IPagination } from '~/interfaces'
+  import CopyLink from '~/components/shared/copy-link.vue'
 
-defineProps({
-  headers: {
-    type: Array as PropType<Header[]>,
-    required: true,
-  },
-  items: {
-    type: Array,
-    required: true,
-  },
-  itemKey: {
+  defineProps({
+    headers: {
+      type: Array as PropType<Header[]>,
+      required: true,
+    },
+    items: {
+      type: Array,
+      required: true,
+    },
+    itemKey: {
+      type: String,
+      default: 'name',
+    },
+    statusHandler: {
+      type: Function as PropType<(item: string) => string>,
+      required: false,
+    },
+    showIndex: {
+      type: Boolean,
+      default: false,
+    },
+    customName: {
+      type: Boolean,
+      default: false,
+    },
+    showFooter: {
+      type: Boolean,
+      default: true,
+    },
+    paginate: {
+      type: Function,
+      required: true,
+    },
+    showLink: {
+      type: Boolean,
+      default: false,
+    },
+    assignTeam: {
+      type: Function as PropType<(item: any) => void>,
+      required: false,
+    },
+    enableAssignTeam: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+  })
+  const search = defineModel('search', {
     type: String,
-    default: 'name',
-  },
-  statusHandler: {
-    type: Function as PropType<(item: string) => string>,
     required: false,
-  },
-  showIndex: {
-    type: Boolean,
-    default: false,
-  },
-  customName: {
-    type: Boolean,
-    default: false,
-  },
-  showFooter: {
-    type: Boolean,
-    default: true,
-  },
-  paginate: {
-    type: Function,
-    required: true,
-  },
-  showLink: {
-    type: Boolean,
-    default: false,
-  },
-  assignTeam: {
-    type: Function as PropType<(item: any) => void>,
-    required: false,
-  },
-  enableAssignTeam: {
-    type: Boolean,
-    default: false,
-    required: false,
-  },
-})
-const search = defineModel('search', {
-  type: String,
-  required: false,
-})
-const pagination = defineModel<IPagination>('pagination', {required: true})
+  })
+  const pagination = defineModel<IPagination>('pagination', { required: true })
 </script>
 <template>
   <v-data-table
-      class="border-sm futzo-rounded"
-      :headers="headers"
-      :items="items"
-      :search="search"
-      :item-key="itemKey"
-      items-per-page="10"
-      show-select
-      :cell-props="{ class: 'text-capitalize' }"
-      height="100%"
+    class="border-sm futzo-rounded"
+    :headers="headers"
+    :items="items"
+    :search="search"
+    :item-key="itemKey"
+    items-per-page="10"
+    show-select
+    :cell-props="{ class: 'text-capitalize' }"
+    height="100%"
   >
     <!--    header-->
-    <template
-        v-slot:header.data-table-select="{ allSelected, selectAll, someSelected }"
-    >
+    <template v-slot:header.data-table-select="{ allSelected, selectAll, someSelected }">
       <v-checkbox-btn
-          :indeterminate="someSelected && !allSelected"
-          :model-value="allSelected"
-          color="primary"
-          @update:model-value="selectAll(!allSelected)"
+        :indeterminate="someSelected && !allSelected"
+        :model-value="allSelected"
+        color="primary"
+        @update:model-value="selectAll(!allSelected)"
       >
       </v-checkbox-btn>
     </template>
@@ -85,19 +83,19 @@ const pagination = defineModel<IPagination>('pagination', {required: true})
     </template>
     <!-- name column-->
     <template v-if="customName" #[`item.name`]="{ item }">
-      <div class="d-flex">
-        <v-avatar :image="item?.image"></v-avatar>
-        <span class="d-inline-block text-truncate" style="max-width:100px"> {{ item?.name }}</span>
+      <div class="d-flex align-center">
+        <v-avatar :image="item?.image" density="compact"> </v-avatar>
+        <span class="d-inline-block text-truncate mx-4" style="max-width: 100px"> {{ item?.name }}</span>
       </div>
     </template>
     <template #[`item.president.email`]="{ item }">
-      <span class="d-inline-block text-truncate" style="max-width:100px"> {{ item?.president?.email }}</span>
+      <span class="d-inline-block text-truncate" style="max-width: 100px"> {{ item?.president?.email }}</span>
     </template>
     <template #[`item.tournament.name`]="{ item }">
-      <span class="d-inline-block text-truncate" style="max-width:100px"> {{ item?.tournament?.name }}</span>
+      <span class="d-inline-block text-truncate" style="max-width: 100px"> {{ item?.tournament?.name }}</span>
     </template>
     <template #[`item.president.phone`]="{ item }">
-      <span class="d-inline-block text-truncate" style="max-width:100px"> {{ item?.president?.phone }}</span>
+      <span class="d-inline-block text-truncate" style="max-width: 100px"> {{ item?.president?.phone }}</span>
     </template>
     <template #[`item.image`]="{ item }">
       <v-avatar size="50" :image="item.image"></v-avatar>
@@ -105,28 +103,21 @@ const pagination = defineModel<IPagination>('pagination', {required: true})
     <template #[`item.birthdate`]="{ item }">
       <v-tooltip :text="item.birthdate.date">
         <template v-slot:activator="{ props }">
-          <span v-bind="props" class="cursor-pointer">
-            {{ item.birthdate.age }}</span
-          >
+          <span v-bind="props" class="cursor-pointer"> {{ item.birthdate.age }}</span>
         </template>
       </v-tooltip>
     </template>
-    <template v-slot:item.team.name="{item}">
-      <v-btn :disabled="!enableAssignTeam"
-             density="compact"
-             v-if="!item?.team?.name"
-             @click="() => assignTeam(item)"
-      >Asignar equipo
+    <template v-slot:item.team.name="{ item }">
+      <v-btn :disabled="!enableAssignTeam" density="compact" v-if="!item?.team?.name" @click="() => assignTeam(item)"
+        >Asignar equipo
       </v-btn>
       <span v-else>{{ item.team.name }}</span>
     </template>
-    <template
-        v-slot:item.data-table-select="{ internalItem, isSelected, toggleSelect }"
-    >
+    <template v-slot:item.data-table-select="{ internalItem, isSelected, toggleSelect }">
       <v-checkbox-btn
-          :model-value="isSelected(internalItem)"
-          color="primary"
-          @update:model-value="toggleSelect(internalItem)"
+        :model-value="isSelected(internalItem)"
+        color="primary"
+        @update:model-value="toggleSelect(internalItem)"
       ></v-checkbox-btn>
     </template>
     <template v-slot:item.status="{ item }">
@@ -141,28 +132,28 @@ const pagination = defineModel<IPagination>('pagination', {required: true})
     </template>
     <!--    link }}-->
     <template v-if="showLink" #[`item.link`]="{ item }">
-      <CopyLink :item="item"/>
+      <CopyLink :item="item" />
     </template>
     <!--    link -->
     <template #bottom>
-      <v-divider/>
+      <v-divider />
       <v-pagination
-          v-if="showFooter"
-          class="futzo-pagination"
-          v-model="pagination.currentPage"
-          :length="pagination.lastPage"
-          start="1"
-          @update:modelValue="() => paginate()"
+        v-if="showFooter"
+        class="futzo-pagination"
+        v-model="pagination.currentPage"
+        :length="pagination.lastPage"
+        start="1"
+        @update:modelValue="() => paginate()"
       >
         <template #prev="props">
           <v-btn
-              @click="props.onClick"
-              :disabled="props.disabled"
-              elevation="0"
-              variant="text"
-              color="black"
-              rounded="md"
-              border="thin secondary"
+            @click="props.onClick"
+            :disabled="props.disabled"
+            elevation="0"
+            variant="text"
+            color="black"
+            rounded="md"
+            border="thin secondary"
           >
             <template #prepend>
               <Icon name="futzo-icon:arrow-left"></Icon>
@@ -172,14 +163,14 @@ const pagination = defineModel<IPagination>('pagination', {required: true})
         </template>
         <template #next="props">
           <v-btn
-              @click="props.onClick"
-              :disabled="props.disabled"
-              elevation="0"
-              variant="text"
-              color="black"
-              rounded="md"
-              border="thin secondary"
-              class="ml-auto"
+            @click="props.onClick"
+            :disabled="props.disabled"
+            elevation="0"
+            variant="text"
+            color="black"
+            rounded="md"
+            border="thin secondary"
+            class="ml-auto"
           >
             <template #append>
               <Icon name="futzo-icon:arrow-right"></Icon>
