deac8216990eda07ccc5f5de2e65162f4cfcfc30
Jose Terrazas <jozaguts@gmail.com>
2025-08-14 19:34:01 -0600

    fix(Dashboard): team link added

--- components/pages/dashboard/last-teams.vue
+++ components/pages/dashboard/last-teams.vue
@@ -1,29 +1,30 @@
 <script lang="ts" setup>
-import {useTeamStore} from "~/store";
-import getHeaders from "~/utils/headers-table";
+  import { useTeamStore } from '~/store'
+  import getHeaders from '~/utils/headers-table'
 
-const headers = getHeaders("teams");
-await useTeamStore().getTeams();
-const {teams, search, pagination} = storeToRefs(useTeamStore());
+  const headers = getHeaders('teams')
+  await useTeamStore().getTeams()
+  const { teams, search, pagination } = storeToRefs(useTeamStore())
 </script>
 <template>
   <Table
-      v-if="teams?.length"
-      :headers="headers"
-      :show-index="true"
-      :items="teams"
-      itemKey="name"
-      :search.sync="search"
-      v-model:pagination="pagination"
-      :paginate="useTeamStore().getTeams"
-      :custom-name="true"
-      :show-footer="false"
+    v-if="teams?.length"
+    :headers="headers"
+    :show-index="true"
+    :items="teams"
+    itemKey="name"
+    :search.sync="search"
+    v-model:pagination="pagination"
+    :paginate="useTeamStore().getTeams"
+    :custom-name="true"
+    :show-footer="false"
+    :show-link="true"
   ></Table>
   <v-empty-state
-      v-else
-      image="/no-data.svg"
-      size="100"
-      text="No hay equipos"
-      title="Últimos equipos inscritos"
+    v-else
+    image="/no-data.svg"
+    size="100"
+    text="No hay equipos"
+    title="Últimos equipos inscritos"
   ></v-empty-state>
 </template>
