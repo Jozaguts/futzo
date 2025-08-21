893053983f56736d0b0b447c2646d0ab210af568
Jose Terrazas <jozaguts@gmail.com>
2025-08-21 00:34:18 -0600

    fea(subscription)  module added

---
+++ app/pages/suscripcion.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  const hydrated = ref(false)
+  definePageMeta({
+    layout: 'blank',
+    bodyAttrs: {
+      class: 'd-none',
+    },
+  })
+  onMounted(() => {
+    hydrated.value = true
+  })
+</script>
+
+<template>
+  <v-container class="mx-auto my-auto" v-if="hydrated">
+    <v-row>
+      <v-col cols="12" class="d-flex justify-center align-center"><div class="dot-collision"></div></v-col>
+    </v-row>
+  </v-container>
+</template>
+<style lang="sass">
+  @use 'three-dots' with (
+      $dot-width: 20px,
+      $dot-height: 20px
+  )
+</style>
