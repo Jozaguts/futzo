6d72db6cd2812437d23c73638eb06dec528bc92f
Jose Terrazas <jozaguts@gmail.com>
2025-09-09 13:07:35 -0600

    feat(stripe): BillingPortal stripe added

--- app/http/api/stripe.ts
+++ app/http/api/stripe.ts
@@ -8,11 +8,34 @@ export type CreatePaymentIntentResponse = {
 
 export const createPaymentIntent = async (
   plan: string,
-  period: 'month' | 'year'
+  period: 'month' | 'year',
+  email?: string
 ): Promise<CreatePaymentIntentResponse> => {
   const client = useSanctumClient();
   return await client<CreatePaymentIntentResponse>(`/api/v1/payment-intents`, {
     method: 'POST',
-    body: { plan, period },
+    body: { plan, period, email },
   });
+};
+export const createSubscriptionIntent = async (
+  plan: string,
+  period: 'month' | 'year',
+  email?: string
+): Promise<CreatePaymentIntentResponse> => {
+  const client = useSanctumClient();
+  return await client<CreatePaymentIntentResponse>(`/api/v1/subscriptions/intent`, {
+    method: 'POST',
+    body: { plan, period, email },
+  });
+};
+
+export type BillingPortalResponse = { url: string };
+
+export const createBillingPortalSession = async (user_id?: number): Promise<BillingPortalResponse> => {
+  let request = '/api/v1/subscriptions/portal';
+  if (user_id) {
+    request = `/api/v1/subscriptions/?user_id=${user_id}`;
+  }
+  const client = useSanctumClient();
+  return await client<BillingPortalResponse>(request);
 };
