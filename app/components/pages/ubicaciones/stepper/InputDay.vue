8f88ff6963c52bad2ef84df92295e55375f1d554
Jose Terrazas <jozaguts@gmail.com>
2025-09-09 20:08:48 -0600

    fix(input time): css missed

--- app/components/pages/ubicaciones/stepper/InputDay.vue
+++ app/components/pages/ubicaciones/stepper/InputDay.vue
@@ -63,7 +63,7 @@
     <v-col v-if="props.day.enabled" cols="12" lg="9" md="9">
       <div class="d-flex">
         <div class="w-100 mx-2">
-          <VueDatePicker time-picker class="custom-dp-location" :is24="true" v-model="startTime">
+          <VueDatePicker time-picker class="custom-dp-location" :is24="true" v-model="startTime" teleport>
             <template #dp-input="{ value }">
               <v-text-field
                 :value="value"
@@ -80,7 +80,7 @@
           </VueDatePicker>
         </div>
         <div class="w-100 mx-2">
-          <VueDatePicker class="custom-dp-location" v-model="endTime" time-picker :is24="true">
+          <VueDatePicker class="custom-dp-location" v-model="endTime" time-picker :is24="true" teleport>
             <template #dp-input="{ value }">
               <v-text-field
                 class="custom-location-input"
@@ -108,4 +108,5 @@
 </template>
 <style lang="sass">
   @use "~/assets/scss/components/input-location-disabled.sass"
+  @use "~/assets/css/vue-datepicker-custom.css"
 </style>
