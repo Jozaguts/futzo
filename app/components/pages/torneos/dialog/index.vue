655c39fc1a5837fd2597f2a8956c401cd06b1372
Jose Terrazas <jozaguts@gmail.com>
2025-09-11 08:20:56 -0600

    feat(dialog): actions added

--- app/components/pages/torneos/dialog/index.vue
+++ app/components/pages/torneos/dialog/index.vue
@@ -6,17 +6,50 @@
   const leaveHandler = () => {
     useTournamentStore().$reset()
   }
+  const disabled = computed(() => {
+    return steps.value.steps[steps.value.current].disable
+  })
+  const nextHandler = () => {}
 </script>
 <template>
-  <v-dialog v-model="dialog" max-width="690" @after-leave="leaveHandler" scrollable>
+  <v-dialog v-model="dialog" max-width="690" min-height="80vh" @after-leave="leaveHandler" scrollable>
     <v-card
       class="create-tournament-card futzo-rounded"
       height="100%"
       :style="{ overflow: $vuetify.display.mobile ? '' : 'hidden' }"
     >
       <HeaderCard />
-
       <StepperContainer :step="steps.current" />
+      <v-card-actions>
+        <v-container>
+          <v-row>
+            <v-col cols="6">
+              <v-btn
+                variant="outlined"
+                block
+                color="secondary"
+                class="text-capitalize"
+                density="comfortable"
+                size="large"
+                @click="() => steps.steps[steps.current].back()"
+                >Anterior
+              </v-btn>
+            </v-col>
+            <v-col cols="6">
+              <v-btn
+                :disabled="disabled"
+                variant="elevated"
+                block
+                color="primary"
+                density="comfortable"
+                size="large"
+                @click="() => steps.steps[steps.current].next()"
+                >Siguiente
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
