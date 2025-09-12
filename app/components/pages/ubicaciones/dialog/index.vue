93ab6b508e11fb8d91ecdd977de758afec910246
Jose Terrazas <jozaguts@gmail.com>
2025-09-12 12:00:37 -0600

    refactor(location): actions added

--- app/components/pages/ubicaciones/dialog/index.vue
+++ app/components/pages/ubicaciones/dialog/index.vue
@@ -7,9 +7,12 @@
     useLocationStore().resetLocationStoreRequest()
     formSteps.value.current = 'location'
   }
+  const disabled = computed(() => {
+    return formSteps.value.steps[formSteps.value.current].disable
+  })
 </script>
 <template>
-  <v-dialog v-model="locationDialog" max-width="690" @after-leave="leaveHandler" scrollable>
+  <v-dialog v-model="locationDialog" max-width="690" min-height="80vh" @after-leave="leaveHandler" scrollable>
     <v-card
       class="create-tournament-card futzo-rounded"
       height="100%"
@@ -17,6 +20,36 @@
     >
       <HeaderCard />
       <LocationStepper @close="() => (locationDialog = false)" @location-added="() => (locationDialog = false)" />
+      <v-card-actions>
+        <v-container>
+          <v-row>
+            <v-col cols="6">
+              <v-btn
+                variant="outlined"
+                block
+                color="secondary"
+                class="text-capitalize"
+                density="comfortable"
+                size="large"
+                @click="() => formSteps.steps[formSteps.current].back()"
+                >Anterior
+              </v-btn>
+            </v-col>
+            <v-col cols="6">
+              <v-btn
+                :disabled="disabled"
+                variant="elevated"
+                block
+                color="primary"
+                density="comfortable"
+                size="large"
+                @click="() => formSteps.steps[formSteps.current].next()"
+                >Siguiente
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
