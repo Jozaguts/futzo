1eb1a33094540d10b9daefc66b560a27611ae9d0
Jose Terrazas <jozaguts@gmail.com>
2025-09-23 14:36:44 -0600

    saving

--- app/composables/useApiError.ts
+++ app/composables/useApiError.ts
@@ -1,14 +1,12 @@
-import {FetchError} from 'ofetch';
-
+import { FetchError } from 'ofetch';
 const VALIDATION_ERROR_CODE = 422;
+
 const SERVER_ERROR_CODE = 500;
 const FORBIDDEN_CODE_ERROR = 403;
-const ERRORS_BAG = [VALIDATION_ERROR_CODE, SERVER_ERROR_CODE, FORBIDDEN_CODE_ERROR]
-
+const ERRORS_BAG = [VALIDATION_ERROR_CODE, SERVER_ERROR_CODE, FORBIDDEN_CODE_ERROR];
 export const useApiError = (error: FetchError) => {
-
-    return {
-        code: error?.response?.status,
-        message: error.data.message,
-    };
+  return {
+    code: error?.response?.status,
+    message: error.data.message,
+  };
 };
--- app/http/api/schedule.ts
+++ app/http/api/schedule.ts
@@ -1,21 +1,41 @@
+import { useSanctumClient } from '#imports';
+import type { ScheduleStoreRequest } from '~/models/Schedule';
+
 export const fetchRoundByStatus = async (tournamentId: number, filter: string, page: number) => {
-    const client = useSanctumClient();
-    type Filters = { filterBy?: string, page: number }
-    let query: Filters = {} as Filters
-    if (filter) {
-        query.filterBy = filter;
-    }
-    if (page) {
-        query.page = page;
-    }
-    try {
-        let url = `/api/v1/admin/tournaments/${tournamentId}/schedule`;
-        const data = await client(url, {
-            query
-        });
-        return data ?? [];
-    } catch (error) {
-        console.error("Error fetching rounds by status:", error);
-        return [];
-    }
-}
+  const client = useSanctumClient();
+  type Filters = { filterBy?: string; page: number };
+  let query: Filters = {} as Filters;
+  if (filter) {
+    query.filterBy = filter;
+  }
+  if (page) {
+    query.page = page;
+  }
+  try {
+    let url = `/api/v1/admin/tournaments/${tournamentId}/schedule`;
+    const data = await client(url, {
+      query,
+    });
+    return data ?? [];
+  } catch (error) {
+    console.error('Error fetching rounds by status:', error);
+    return [];
+  }
+};
+export const generateSchedule = async (tournamentId: number, data: ScheduleStoreRequest) => {
+  const client = useSanctumClient();
+  if (data.elimination_phase.group_phase) {
+    const group_phase = { ...data.elimination_phase.group_phase };
+    console.log({ group_phase });
+    delete data.elimination_phase.group_phase;
+    data = {
+      ...data,
+      group_phase,
+    };
+  }
+  console.log(data);
+  return await client(`/api/v1/admin/tournaments/${tournamentId}/schedule`, {
+    method: 'POST',
+    body: JSON.stringify(data),
+  });
+};
