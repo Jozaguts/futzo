b418e1eb9942e40cadbc16cf946a6de18ad2dcc6
Jose Terrazas <jozaguts@gmail.com>
2025-10-09 17:26:49 -0600

    refactor(schedule): hide on mobile

--- app/components/pages/jugadores/players-navbar-buttons.vue
+++ app/components/pages/jugadores/players-navbar-buttons.vue
@@ -1,12 +1,14 @@
 <script lang="ts" setup>
-import CATBtn from "@/components/pages/jugadores/app-bar-cat-btn.vue";
-import SearchInput from "@/components/pages/jugadores/app-bar-search-input.vue";
-import ImportBtn from "@/components/pages/jugadores/import-btn.vue";
-import ExportBtn from "@/components/pages/jugadores/export-btn.vue";
+  import CATBtn from '@/components/pages/jugadores/app-bar-cat-btn.vue'
+  import SearchInput from '@/components/pages/jugadores/app-bar-search-input.vue'
+  import ImportBtn from '@/components/pages/jugadores/import-btn.vue'
+  import ExportBtn from '@/components/pages/jugadores/export-btn.vue'
 </script>
 <template>
-  <SearchInput/>
-  <ImportBtn/>
-  <!--  <ExportBtn />-->
-  <CATBtn/>
+  <div class="d-none d-md-flex d-lg-flex">
+    <SearchInput />
+    <ImportBtn />
+    <!--  <ExportBtn />-->
+    <CATBtn />
+  </div>
 </template>
