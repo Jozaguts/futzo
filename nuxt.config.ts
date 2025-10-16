c400e4167608271692feabbfd7ad64a680f2e9da
Jose Terrazas <jozaguts@gmail.com>
2025-10-15 18:33:55 -0600

    saving

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 import path from 'node:path';
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   app: {
     head: {
       meta: [
