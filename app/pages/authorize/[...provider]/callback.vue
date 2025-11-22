279f72b1fcb4a8e2f3fa0e8d32608830aa1f1a5a
Jose Terrazas <jozaguts@gmail.com>
2025-11-22 15:35:52 -0600

    fix(auto provider): callback removed

--- app/pages/authorize/[...provider]/callback.vue
+++ app/pages/authorize/[...provider]/callback.vue
@@ -12,36 +12,20 @@
   })
 
   onMounted(() => {
-    const route = useRoute()
-    const provider = route?.params?.provider[0]
-    console.log(provider)
-    console.log(route.query?.code)
-    console.log(route.query)
-    console.log(route.params)
-    console.log(route)
-    if (provider === 'google' || provider === 'facebook') {
-      const client = useSanctumClient()
-
-      client(`/auth/${provider}/callback`, {
-        credentials: 'include',
-        params: { code: route.query.code },
-      })
-        .then(() => {
-          const { refreshIdentity, isAuthenticated } = useSanctumAuth()
-          refreshIdentity()
-            .catch((error) => console.error(error))
-            .then(() => {
-              if (isAuthenticated.value) {
-                console.log('user authenticated')
-                useRouter().push('/')
-              } else {
-                console.log('user not authenticated')
-              }
-            })
-        })
+    try {
+      const { refreshIdentity, isAuthenticated } = useSanctumAuth()
+      refreshIdentity()
         .catch((error) => console.error(error))
-    } else {
-      console.error('provider not found')
+        .then(() => {
+          if (isAuthenticated.value) {
+            console.log('user authenticated')
+            useRouter().push('/')
+          } else {
+            console.log('user not authenticated')
+          }
+        })
+    } catch (error) {
+      console.log(error)
     }
   })
 </script>
