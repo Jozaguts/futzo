8096d1c35bb4c5f008dc7c431a8d6fb0dfbbb15a
Jose Terrazas <jozaguts@gmail.com>
2025-11-22 13:52:20 -0600

    test(autorize callback): missing code

--- app/pages/authorize/[...provider]/callback.vue
+++ app/pages/authorize/[...provider]/callback.vue
@@ -4,38 +4,43 @@
   </div>
 </template>
 <script setup lang="ts">
+  definePageMeta({
+    layout: 'blank',
+    sanctum: {
+      excluded: true,
+    },
+  })
 
-definePageMeta({
-  layout: "blank",
-  sanctum: {
-    excluded: true,
-  },
-});
+  onMounted(() => {
+    const route = useRoute()
+    const provider = route.params.provider[0]
 
-onMounted(() => {
-  const provider = useRoute().params.provider[0];
-  if (provider === "google" || provider === "facebook") {
-    const client = useSanctumClient();
-    client(`/auth/${provider}/callback`, {
-      credentials: "include",
-      params: {code: useRoute().query.code},
-    })
+    if (provider === 'google' || provider === 'facebook') {
+      const client = useSanctumClient()
+      console.log(route.query?.code)
+      console.log(route.query)
+      console.log(route.params)
+      console.log(route)
+      client(`/auth/${provider}/callback`, {
+        credentials: 'include',
+        params: { code: route.query.code },
+      })
         .then(() => {
-          const {refreshIdentity, isAuthenticated} = useSanctumAuth();
+          const { refreshIdentity, isAuthenticated } = useSanctumAuth()
           refreshIdentity()
-              .catch((error) => console.error(error))
-              .then(() => {
-                if (isAuthenticated.value) {
-                  console.log("user authenticated");
-                  useRouter().push("/");
-                } else {
-                  console.log("user not authenticated");
-                }
-              });
+            .catch((error) => console.error(error))
+            .then(() => {
+              if (isAuthenticated.value) {
+                console.log('user authenticated')
+                useRouter().push('/')
+              } else {
+                console.log('user not authenticated')
+              }
+            })
         })
-        .catch((error) => console.error(error));
-  } else {
-    console.error("provider not found");
-  }
-});
+        .catch((error) => console.error(error))
+    } else {
+      console.error('provider not found')
+    }
+  })
 </script>
