94306853457a024d1d1fef94cc557a64bbd4dc72
Jose Terrazas <jozaguts@gmail.com>
2026-01-28 17:06:40 -0600

    feat(ui): integrate vue3-easy-data-table and register component globally
    Added `vue3-easy-data-table` dependency to `package.json`. Registered the `e-data-table` component globally in Nuxt with `easy-table.client.ts` plugin. Updated `yarn.lock` with new dependencies.

---
+++ app/plugins/easy-table.client.ts
@@ -0,0 +1,5 @@
+import Vue3EasyDataTable from 'vue3-easy-data-table';
+import 'vue3-easy-data-table/dist/style.css';
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.component('e-data-table', Vue3EasyDataTable);
+});
